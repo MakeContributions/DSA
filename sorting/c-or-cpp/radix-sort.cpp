819c79f71bde0b3690cac968aa35021e29d400db
Tawfik Yasser <tawfekyassertawfek@gmail.com>
2021-02-06 16:05:27 +0200

    Update radix-sort.cpp
    Update code to accept all test cases

--- sorting/c-or-cpp/radix-sort.cpp
+++ sorting/c-or-cpp/radix-sort.cpp
@@ -2,28 +2,42 @@
 #include <iostream>
 #include<algorithm>
 using namespace std;
-void countingSort(int arr[],int n,int pos){
-    int countArr[10];
-    int sortedArr[n];
-    for(int i = 0 ; i < 10 ; i++)
-        countArr[i] = 0;
-    for(int i = 0 ; i < n ; i++)
-        countArr[(arr[i]/pos)%10]++;
-    for(int i = 1 ; i < n ; i++)
-        countArr[i] = countArr[i] + countArr[i-1];
-    for(int i = n - 1 ; i >= 0 ; i--)
-        sortedArr[--countArr[(arr[i]/pos)%10]] = arr[i];
-    for(int i = 0 ; i < n ; i++)
-        arr[i] = sortedArr[i];
+int getMax(int array[], int n) {
+  int max = array[0];
+  for (int i = 1; i < n; i++)
+    if (array[i] > max)
+      max = array[i];
+  return max;
 }
-void radixSort(int arr[],int n){
+void countingSort(int array[], int size, int place) {
+  const int max = 10;
+  int output[size];
+  int count[max];
 
-    int maxElement = *max_element(arr, arr + n);
-    for(int pos = 1 ; maxElement/pos > 0 ; pos*=10){
-        countingSort(arr,n,pos);
-    }
+  for (int i = 0; i < max; ++i)
+    count[i] = 0;
 
+  for (int i = 0; i < size; i++)
+    count[(array[i] / place) % 10]++;
+
+  for (int i = 1; i < max; i++)
+    count[i] += count[i - 1];
+
+  for (int i = size - 1; i >= 0; i--) {
+    output[count[(array[i] / place) % 10] - 1] = array[i];
+    count[(array[i] / place) % 10]--;
+  }
+
+  for (int i = 0; i < size; i++)
+    array[i] = output[i];
 }
+
+void radixsort(int array[], int size) {
+  int max = getMax(array, size);
+  for (int place = 1; max / place > 0; place *= 10)
+    countingSort(array, size, place);
+}
+
 //Print the sorted array
 void printSortedArray(int arr[],int n){
     for(int i=0;i<n;i++){
