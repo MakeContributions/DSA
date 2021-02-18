a38cd29ba209d0836d680a64c26b67920a76fa09
Atin Bainada <atinbainada1710@gmail.com>
2021-02-18 21:31:38 +0530

    algo to count inversions in array
    Added in C++, Python, JavaScript and Java

---
+++ arrays/README.md
@@ -0,0 +1,17 @@
+# Algorithms related to arrays
+
+### C or C++
+
+1. [Counting Inversions](c-or-cpp/count-inversions.cpp)
+
+### Python
+
+1. [Counting Inversions](python/count-inversions.py)
+
+### JavaScript
+
+1. [Counting Inversions](js/count-inversions.js)
+
+### Java
+
+1. [Counting Inversions](java/count-inversions.java)
---
+++ arrays/python/count-inversions.py
@@ -0,0 +1,40 @@
+def count_split_inv(arr, left, right):
+  split_inv = ridx = lidx = 0
+  size = len(arr)
+  lsize = len(left)
+  rsize = len(right)
+  for i in range(size):
+    if lidx != lsize and ridx != rsize:
+      if right[ridx] <= left[lidx]:
+        arr[i] = right[ridx]
+        ridx += 1
+        split_inv += lsize - lidx
+      else:
+        arr[i] = left[lidx]
+        lidx += 1
+    elif lidx == lsize:
+      arr[i] = right[ridx]
+      ridx += 1
+    elif ridx == rsize:
+      arr[i] = left[lidx]
+      lidx += 1
+  return split_inv;
+
+def count_inversions(arr):
+  size = len(arr)
+  if size == 1:
+    return 0
+  mid = int(size/2)
+  left = arr[:mid]
+  right = arr[mid:]
+  l_inv = count_inversions(left)
+  r_inv = count_inversions(right)
+  split_inv = count_split_inv(arr, left, right)
+
+  return l_inv + r_inv + split_inv
+
+with open("inp.txt") as f:
+  lines = f.readlines()
+  numbers = [int(line) for line in lines]
+
+print(count_inversions(numbers))
