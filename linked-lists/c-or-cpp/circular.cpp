eb3c1ffc8d198aa44c8b639176ddb911966ce637
Visrut <visrutnavadiya111@gmail.com>
2021-02-27 20:50:02 +0530

    Update circular.cpp

--- linked-lists/c-or-cpp/circular.cpp
+++ linked-lists/c-or-cpp/circular.cpp
@@ -1,39 +1,102 @@
 #include <iostream>
-using namespace std;
-struct Node {
-   int data;
-   struct Node *next;
+
+template <typename T>
+class Node {
+    public:
+        T data;
+        Node<T>* next;
+
+        public:
+            // this create node with data
+            Node(int data)
+                : data(data) , next(nullptr) {}
 };
-struct Node* head = NULL;
-void insert(int newdata) {
-   struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
-   struct Node *ptr = head;
-   newnode->data = newdata;
-   newnode->next = head;
-   if (head!= NULL) {
-      while (ptr->next != head)
-      ptr = ptr->next;
-      ptr->next = newnode;
-   } else
-   newnode->next = newnode;
-   head = newnode;
-}
-void display() {
-   struct Node* ptr;
-   ptr = head;
-   do {
-      cout<<ptr->data <<" ";
-      ptr = ptr->next;
-   } while(ptr != head);
-}
+
+template <typename T>
+class CircularLinkedList {
+    private:
+        Node<T>* head;
+        Node<T>* tail;
+
+    public:
+        // initially head and tail must be null
+        CircularLinkedList()
+            : head(nullptr) , tail(nullptr) {}
+
+        void insertAtHead(T data) {
+            Node<T> *newNode = new Node<T>(data);
+
+            // if list is empty add new node
+            if(this->head == nullptr) {
+                this->head = newNode;
+                this->tail = newNode;
+                newNode->next = this->head;
+                return;
+            }
+
+            newNode->next = this->head;
+            this->head = newNode;
+            this->tail->next = newNode;
+        }
+
+        void insertAtEnd(T data) {
+            Node<T>* newNode = new Node<T>(data);
+
+            // if list is empty add new node
+            if(this->tail == nullptr) {
+                this->head = newNode;
+                this->tail = newNode;
+                newNode->next = this->head;
+                return;
+            }
+
+            this->tail->next = newNode;
+            newNode->next = this->head;
+            this->tail = newNode;
+        }
+
+        T deleteAtHead() {
+            if(this->head == nullptr) {
+                std::cout<<"Stack Underflow"<<std::endl;
+                exit(1);
+            }
+
+            Node<T>* temp = this->head;
+            T data = this->head->data;
+            if(this->head == this->head->next) {
+                this->head = nullptr;
+                this->tail = nullptr;
+                delete temp;
+                return data;
+            }
+
+            this->head = this->head->next;
+            tail->next = this->head;
+            delete temp;
+            return data;
+        }
+
+        void printList() const {
+            Node<T>* itr = this->head;
+            if(itr == nullptr) {
+                std::cout<<"List is Empty";
+            } else {
+                // print every element after 1s since it's circular
+                while(itr != this->tail) {
+                    std::cout<<itr->data<<" "<<std::flush;
+                    itr = itr->next;
+                }
+                std::cout<<itr->data;
+            }
+            std::cout<<std::endl;
+        }
+};
+
 int main() {
-   insert(3);
-   insert(1);
-   insert(7);
-   insert(3);
-   insert(1);
-   insert(7);
-   cout<<"The circular linked list is: ";
-   display();
-   return 0;
+    CircularLinkedList<int> c;
+    c.insertAtHead(1);
+    c.insertAtEnd(2);
+    c.insertAtEnd(3);
+    c.printList();
+    return 0;
 }
