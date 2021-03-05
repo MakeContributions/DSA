e5d1b5e2cdf709c21032cd923fcaf69b4b08dfb5
Tawfik <tawfekyassertawfek@gmail.com>
2021-03-05 17:18:27 +0200

    Queue linked-.list Added

---
+++ queues/README.md
@@ -0,0 +1,5 @@
+# Queues
+
+### C or C++
+
+1. [Queue using linked list](c-or-cpp/queue-linked-list.cpp)
---
+++ queues/c-or-cpp/queue-linked-list.cpp
@@ -0,0 +1,119 @@
+// Content: Queue using linked list (FIFO)
+// Author: Tawfik Yasser
+// Date: Fri, 5 Mar 2021
+
+#include <iostream>
+using namespace std;
+// Class `Node` to represent every item in the queue
+class Node
+{
+public:
+    int data;    // Item data
+    Node *next;  // Pointer to next item
+    Node() {}    // Empty constructor
+};
+// Claas to represent the queue using linked list
+class QueueLL
+{
+private:
+    Node *front; // Pointer to the head of the queue
+    Node *rear;  // Pointer to the end of the queue
+    int _length;
+public:
+    QueueLL() //Empty constructor to initialize front and rear to NULL because in the start the queue will be empty
+    {
+        front = rear = NULL;
+    }
+
+    // Function `Enqueue` to add new item to the queue from the rear side (End)
+    void enqueue(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = NULL;
+        if(front == NULL && rear == NULL) // Empty queue
+        {
+            front = rear = newNode;
+        }
+        else
+        {
+            rear->next = newNode;
+            rear = newNode;
+        }
+    }
+ 
+    // Function `Dequeue` to remove an item from the queue from the front side (Start)
+    void dequeue()
+    {
+        Node *temp = front;
+        front = front->next;
+        delete temp; //Free the memory
+    }
+
+    // Function `Peek` to return the first item in the queue
+    void peek()
+    {
+        cout<<front->data<<endl;
+    }
+
+    // Function `Print` to display the items of the queue
+    void print()
+    {
+        if(front == NULL && rear == NULL) //Empty queue
+        {
+            cout<<"Empty Queue"<<endl;
+        }
+        else
+        {
+            Node *temp = front;
+            while(temp != NULL) // Till the end of the queue
+            {
+                cout<<"[ "<<temp->data<<" ] ";
+                temp = temp->next;
+            }
+        }
+    }
+
+
+    // Function to calculate the size of the queue (The actual elments in the queue)
+    void calcSize()
+    {
+
+        if(front == NULL && rear == NULL) // Empty queue
+        {
+            _length = 0;
+        }
+        else
+        {
+            int _count;
+            Node *temp = front;
+            while(temp!= NULL)
+            {
+                temp = temp->next;
+                _count++;
+            }
+            _length = _count;
+        }
+    }
+    int _size()
+    {
+        calcSize();
+        return _length;
+    }
+};
+int main()
+{
+    //Test the functions
+    QueueLL qll;
+    qll.enqueue(5);
+    qll.enqueue(3);
+    qll.enqueue(-2);
+    qll.print();
+    cout<<endl;
+    qll.peek();
+    qll.dequeue();
+    qll.print();
+    cout<<endl;
+    qll.peek();
+    return 0;
+}
