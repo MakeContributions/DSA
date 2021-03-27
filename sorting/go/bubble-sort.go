878f2b3d46f96bedfabbae0e5f7fbfc85f519416
UnleashMe69 <57689207+UnleashMe69@users.noreply.github.com>
2021-03-27 17:59:41 +0100

    Updated bubble sort algorithm

--- sorting/go/bubble-sort.go
+++ sorting/go/bubble-sort.go
@@ -1,73 +1,28 @@
+/* 
+    Bubble sort is a recursive algorithm based on swapping 2 values closest to each other: k and k+1
+    We start by checking the first two values of the array, and we swap them if the first value is bigger than the second one.
+    Then we recursively swap the second value with the third, if needed and so on until the array is sorted.
+    Average Time Complexity: O(n^2))
+*/
+
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"math"
-	"os"
-	"strconv"
-	"strings"
 )
 
-//Swaps two values of a slice
-func Swap(sliceInt []int, indexValue int) {
-	//Treats edge case
-	if indexValue < len(sliceInt)-1 {
-		tempValue := sliceInt[indexValue]
-		sliceInt[indexValue] = sliceInt[indexValue+1]
-		sliceInt[indexValue+1] = tempValue
-	}
-}
-
-//Recursive implementation of Bubble sort
-func bubbleSort(sliceInt []int) {
-	for k := 0; k < len(sliceInt)-1; k++ {
+func bubbleSort(sliceInt []int, n int) {
+	for k := 0; k < n-1; k++ {
 		if sliceInt[k] > sliceInt[k+1] {
-			Swap(sliceInt, k)
-			bubbleSort(sliceInt)
+			// We swaps the value if sliceInt[k] > sliceInt[k+1]
+			sliceInt[k], sliceInt[k+1] = sliceInt[k+1], sliceInt[k]
+			bubbleSort(sliceInt, n)
 		}
 	}
 }
 
-func check(e error) {
-	if e != nil {
-		fmt.Println("error: ", e)
-	}
-}
-
-func userInput() []int {
-
-	sliceInt := make([]int, 0, 10)
-	//Takes input from user
-	fmt.Println("Type in a sequence of up to 10 integers separated by a space:")
-	reader := bufio.NewReader(os.Stdin)
-	scanner, err := reader.ReadString('\n')
-	check(err)
-
-	//Trims \r and \n from the user input
-	scanner = strings.TrimSuffix(scanner, "\n")
-	scanner = strings.TrimSuffix(scanner, "\r")
-
-	//Splits integers and appends to sliceInt
-	splitInput := strings.Split(scanner, " ")
-
-	for i := 0; i < int(math.Min(10.0, float64(len(splitInput)))); i++ {
-		userInput, err := strconv.Atoi(splitInput[i])
-		check(err)
-		sliceInt = append(sliceInt, userInput)
-	}
-	return sliceInt
-}
-
 func main() {
-
-	//Calls userInput
-	sliceInt := userInput()
-
-	//Calls bubbleSort
-	bubbleSort(sliceInt)
-
-	//Prints the slice of integers in sorted order
+	sliceInt := []int{1, 2, -1, 0, 534, -100, 9, 53, 203}
+	bubbleSort(sliceInt, len(sliceInt))
 	fmt.Println(sliceInt)
-
 }
