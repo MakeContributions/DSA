61c8efd4cc568523b9b7404d2db6c3da04cd6823
Christian Clauss <cclauss@me.com>
2021-04-09 23:26:10 +0200

    GitHub Action to lint Python code

--- sorting/python/bubble-sort.py
+++ sorting/python/bubble-sort.py
@@ -1,15 +1,21 @@
-def bubbleSort(arr): 
-	n = len(arr) 
+arr = [23, 34, 25, 12, 54, 11, 90]
 
-	for i in range(n-1): 
-		for j in range(0, n-i-1): 
-			if arr[j] > arr[j+1] : 
-				arr[j], arr[j+1] = arr[j+1], arr[j] 
 
-arr = [23, 34, 25, 12, 54, 11, 90] 
+def bubbleSort(arr):
+    """
+    >>> bubbleSort(arr)
+    [11, 12, 23, 25, 34, 54, 90]
+    """
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
-bubbleSort(arr) 
 
-print ("Sorted array is:") 
-for i in range(len(arr)): 
-	print (arr[i]) 
+bubbleSort(arr)
+
+print("Sorted array is:")
+for i in range(len(arr)):
+    print(arr[i])
