8a15e37df1c0daa569e4dbd51117391f5526d559
Christian Clauss <cclauss@me.com>
2021-04-11 20:52:24 +0200

    Update insertion-sort.py

--- sorting/python/insertion-sort.py
+++ sorting/python/insertion-sort.py
@@ -17,7 +17,7 @@ def insertion_sort(arr):
 	"""
 	for j in range(1, len(arr)):
 		key = arr[j]
-		#insert the key into the sorted sequence arr[1, ... , j - 1]
+		# insert the key into the sorted sequence arr[1, ... , j - 1]
 		i = j - 1
 		while i >= 0 and arr[i] > key:
 			arr[i + 1] = arr[i]
