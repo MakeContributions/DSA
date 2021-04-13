9a3ad7ade9e7f570b14539a2d26fe3b6a8f00b46
Venkata Sridhar Sai <65086710+sridhar-5@users.noreply.github.com>
2021-04-13 23:25:32 +0530

    comments wrapped,Thanks for the suggestion.

--- stacks/JAVA/The_Stock_Span_Problem.java
+++ stacks/JAVA/The_Stock_Span_Problem.java
@@ -6,10 +6,13 @@ import java.util.Stack ;
 
 /**
  * Problem statement :
- * The stock span problem is a financial problem where we have a series of n daily price quotes for a stock and we need to calculate span of stock’s price for all n days.
- * The span Si of the stock’s price on a given day i is defined as the maximum number of consecutive days just before the given day, for which the price of the stock on
+ * The stock span problem is a financial problem where we have a series of n daily price quotes for a stock and we need to calculate span of stock’s price 
+ * for all n days.
+ * The span Si of the stock’s price on a given day i is defined as the maximum number of consecutive days just before the given day, 
+ * for which the price of the stock on
  * the current day is less than or equal to its price on the given day.
- * For example, if an array of 7 days prices is given as {100, 80, 60, 70, 60, 75, 85}, then the span values for corresponding 7 days are {1, 1, 1, 2, 1, 4, 6}
+ * For example, if an array of 7 days prices is given as {100, 80, 60, 70, 60, 75, 85}, then the span values for corresponding 
+ * 7 days are {1, 1, 1, 2, 1, 4, 6}
  */
 class The_Stock_Span_Problem extends stack{
 
