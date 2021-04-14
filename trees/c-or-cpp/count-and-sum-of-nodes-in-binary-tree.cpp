305558ba25112be01c6a9090a1a8531cf12603c7
Raghav Goel <raghavgoel847@gmail.com>
2021-04-14 23:08:36 +0530

    Done all required changes

--- trees/c-or-cpp/count-and-sum-of-nodes-in-binary-tree.cpp
+++ trees/c-or-cpp/count-and-sum-of-nodes-in-binary-tree.cpp
@@ -1,72 +1,86 @@
 /*
 Problem
 
-Count the Number of Nodes in the Binarry Tree, 
-and do find the sum of all nodes in a binary tree.
+Count the Number of Nodes in the binary tree, 
+and to find the sum of all nodes in a binary tree.
 
 */
 
 #include <iostream>
-
 using namespace std;
 
 // Basic Structure of a tree
 class Node
 {
-public:
+private:
     int data;
     Node *left;
     Node *right;
 
+public:
     Node(int value)
     {
         data = value;
         left = NULL;
         right = NULL;
     }
-};
 
-/* Counting Nodes Algorithm
+    void insert(Node *&root, int data)
+    {
+
+        if (root == NULL)
+            root = new Node(data);
+        else if (root->data > data)
+            insert(root->left, data);
+        else
+            insert(root->right, data);
+    }
+
+    /* Counting Nodes Algorithm
 
     Left subtree Count + Right Subtree Count + 1
 
-*/
+    */
 
-int CountNodes(Node *root)
-{
-    //Base Case
-    if (root == NULL)
-        return 0;
+    int CountNodes(Node *&root)
+    {
+        //Base Case
+        if (root == NULL)
+            return 0;
 
-    return CountNodes(root->left) + CountNodes(root->right) + 1;
-}
+        return CountNodes(root->left) + CountNodes(root->right) + 1;
+    }
 
-/* Sum of Nodes Nodes 
+    /* Sum of Nodes Nodes 
     Left subtree Sum + Right Subtree Sum + Node Value
-*/
+    */
 
-int SumNodes(Node *root)
-{
-    //Base Case
-    if (root == NULL)
-        return 0;
+    int SumNodes(Node *&root)
+    {
+        //Base Case
+        if (root == NULL)
+            return 0;
 
-    return SumNodes(root->left) + SumNodes(root->right) + root->data;
-}
+        return SumNodes(root->left) + SumNodes(root->right) + root->data;
+    }
+};
 
 int main()
 {
-
-    Node *root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
-    root->left->left = new Node(4);
-    root->left->right = new Node(5);
-    root->right->left = new Node(6);
-    root->right->right = new Node(7);
+    //User Input
+    Node *root = NULL;
+    int n, num;
+    cout << "Enter the Number of nodes";
+    cin >> n;
+    cout << "Enter the value of the nodes";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> num;
+        root->insert(root, num);
+    }
 
     //Counting nodes
-    cout << "Number of Nodes : " << CountNodes(root) << endl;
+    cout << "Number of Nodes : " << root->CountNodes(root) << endl;
 
     /*                       
                     1
@@ -85,7 +99,7 @@ int main()
     */
 
     //Sum of Nodes
-    cout << "Sum of Nodes : " << SumNodes(root);
+    cout << "Sum of Nodes : " << root->SumNodes(root);
 
     /*                       
                     1
