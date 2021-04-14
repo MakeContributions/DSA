ffbd22e90096d0040146113a432b99c2ae4d3e0c
JACOBIN-SCTCS <jacobja3@gmail.com>
2021-04-14 23:09:09 +0530

    Added changes to Dijkstras Algorithm Implementation according to review

--- graphs/java/Dijkstras.java
+++ graphs/java/Dijkstras.java
@@ -1,11 +1,9 @@
 import java.util.*;
 
-
 class AdjListNode
 {
 	int dest;
 	int weight;
-
 	AdjListNode(int dest,int weight)
 	{
 		this.dest=dest;
@@ -13,16 +11,11 @@ class AdjListNode
 	}
 }
 
-
-
-
-
-
-class NodeComparator implements Comparator<AdjListNode>{
+class NodeComparator implements Comparator<AdjListNode>
+{
 	@Override
 	public int compare(AdjListNode node1, AdjListNode node2) 
-	{
-		
+	{		
 		if (node1.weight < node2.weight) 
             		return -1; 
         	if (node1.weight > node2.weight) 
@@ -31,52 +24,31 @@ class NodeComparator implements Comparator<AdjListNode>{
 	}
 }
 
-
-
-
-
-
-
 class Dijkstras
 {
-
 	public int[] dijkstras(List<List<AdjListNode>> adj_list,int source)
 	{
 		int[] distance = new int[adj_list.size()];
 		Set<Integer> completed = new HashSet<Integer>();
-
 		for(int i=0;i<distance.length;++i)
 		{
 			distance[i] = Integer.MAX_VALUE;
-			
 		}
-
-
-
 		PriorityQueue<AdjListNode> pq = new PriorityQueue<>(adj_list.size(),new NodeComparator());
 		AdjListNode source_node = new AdjListNode(source,0);
-		
 		for(int i=0;i<distance.length;++i)
 		{
 			pq.add(new AdjListNode(i,Integer.MAX_VALUE));
 			
 		}
-
-		
 		pq.add(source_node);
 		distance[source]=0;
-
-
-
-
 		while(!pq.isEmpty())
 		{
 			AdjListNode current = pq.remove();
-			
 			if(!completed.contains(current.dest))
 			{
 				completed.add(current.dest);
-				
 				for(AdjListNode n :adj_list.get(current.dest) )
 				{
 					if( distance[n.dest] > (distance[current.dest]+n.weight))
@@ -85,63 +57,71 @@ class Dijkstras
 						pq.add(new AdjListNode(n.dest, distance[n.dest]));
 					}
 				}
-
-
-
 			}
-
 		}
-
 		return distance;
 	}
 
-
-
-
-
-
-
-	
-
-	public static List<List<AdjListNode>> createGraph()
-	{
-		List<List<AdjListNode>> adj_list = new ArrayList<List<AdjListNode>>();
-		
-		for(int i=0;i<6;++i)
-		{
-			adj_list.add(new ArrayList<AdjListNode>());
-
-		}
-		
-		adj_list.get(0).add(new AdjListNode(1, 1));
-		adj_list.get(0).add(new AdjListNode(2, 5));
-		adj_list.get(1).add(new AdjListNode(2, 2));
-		adj_list.get(1).add(new AdjListNode(3, 2));
-		adj_list.get(1).add(new AdjListNode(4, 1));
-		adj_list.get(2).add(new AdjListNode(4, 2));
-		adj_list.get(3).add(new AdjListNode(4, 3));
-		adj_list.get(3).add(new AdjListNode(5, 1));
-		adj_list.get(4).add(new AdjListNode(5, 2));
-
-
-		
-		return adj_list;
-
-
-	}
-
-
-
-
-
 	public static void main(String args[])
 	{
-	
-		List<List<AdjListNode>> adj_list = createGraph(); // sample graph You can  write custom I/O to get adjacency list
-		Dijkstras d = new Dijkstras();
-		int[] distances  = d.dijkstras(adj_list, 0);  //source vertex is taken as 0
+		/*
+		  	SAMPLE INPUT AND OUTPUT
+			___________________________
+
+			Input:
+			__________
+			Please Enter the number of nodes N. Vertices will be [0,N-1]
+			6
+			Please Enter the number of Edges
+			9
+			Please Enter each edge in the sequence <starting node> <destination node> <weigth>
+			0 1 1
+			0 2 5
+			1 2 2
+			1 4 1
+			1 3 2
+			2 4 2
+			3 5 1
+			3 4 3
+			4 5 2
+			Please Enter Source Vertex
+			0
+
+			Output:
+			___________
+			Distances from source 0
+			Node0--->0
+			Node1--->1
+			Node2--->3
+			Node3--->3
+			Node4--->2
+			Node5--->4
+		 */
+
+		List<List<AdjListNode>> adj_list = new ArrayList<List<AdjListNode>>();
+		System.out.println("Please Enter the number of nodes N. Vertices will be [0,N-1]");
+		Scanner sc = new Scanner(System.in);
+		int v = sc.nextInt();
+		for(int i=0;i<v;++i)
+			adj_list.add(new ArrayList<AdjListNode>());
 		
-		System.out.println("Distances from source 0");
+		System.out.println("Please Enter the number of Edges");
+		int e = sc.nextInt();
+		System.out.println("Please Enter each edge in the sequence <starting node> <destination node> <weigth>");
+		// Sample Data: 0 2 5 (edge from 0 to 2 with weight 5)
+		for(int i=0;i<e;++i)
+		{
+			int startnode = sc.nextInt();
+			int destnode = sc.nextInt();
+			int weight = sc.nextInt();
+		       	adj_list.get(startnode).add(new AdjListNode(destnode,weight));
+		}
+		int source;
+		System.out.println("Please Enter Source Vertex");
+		source = sc.nextInt();
+		Dijkstras d = new Dijkstras();
+		int[] distances  = d.dijkstras(adj_list, source);  //source vertex is taken as 0
+		System.out.println("Distances from source "+source);
 		for(int i=0;i<distances.length;++i)
 		{
 			if(distances[i]==Integer.MAX_VALUE)
@@ -149,6 +129,5 @@ class Dijkstras
 			else
 				System.out.println("Node"+i+"--->"+distances[i]);
 		}
-
 	}
 }
