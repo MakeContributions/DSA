2b3b760081b2d798621ec7aac716d9b053293660
Manisha <76945004+MannyP31@users.noreply.github.com>
2021-04-15 12:51:57 +0530

    Added some basic Cpp programs

---
+++ vdec.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// Variable declaration:
+extern int a,b;
+extern int c;
+extern float f;
+
+int main(){
+	 //Variable defination:
+	 int a,b;
+	 int c;
+	 float f;
+	 
+	 //actual initialization
+	 a=10;
+	 b=20;
+	 c= a + b;
+	 
+	 cout << c << endl ;
+	 
+	 f=70.0/3.0;
+	 cout << f << endl;
+	 
+	 return 0;
+}
