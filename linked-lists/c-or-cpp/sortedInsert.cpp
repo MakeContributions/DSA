0848253d35978f4316c4baada8ffec2d10be1834
LightMonarch <rahulpillai0128@gmail.com>
2021-04-15 22:11:55 +0530

    Fix requested changes

--- linked-lists/c-or-cpp/sortedInsert.cpp
+++ linked-lists/c-or-cpp/sortedInsert.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
-#include <time.h>
+#include <ctime>
 
 template <typename T>
 class LinkedList{
 	public:
 		void push(const T& data);
 		void display();
-		void sortedInsert(const bool& reverse = false);
 		void pushfront(const T& data);
+		bool sortInsertRev = false;
 
 	private:
-		bool reverse_;
-		bool sortInsertInit_ = false;
-		void sortedInsertALGO();
 		struct node{
 			T data;
 			node* next;
@@ -20,25 +17,6 @@ class LinkedList{
 		};
 		node* head_ = nullptr;
 };
-
-template <typename T>
-void LinkedList<T>::sortedInsert(const bool& reverse){
-	/*
-	Sorts the list in either order as it inserts new element to the list
-
-	@params: reverse
-		false(default) - for Ascending order
-		true - for Descending order
-	*/
-	
-	if(this->head_ != nullptr){
-		std::cout << "Can only be called before pushing any element to the list" << std::endl;	
-		return;
-	}
-
-	reverse_ = reverse;
-	sortInsertInit_ = true;
-}
 
 
 template <typename T>
@@ -50,10 +28,6 @@ void LinkedList<T>::pushfront(const T& data){
 
 template <typename T>
 void LinkedList<T>::push(const T& data){
-	if(!sortInsertInit_){
-		std::cout << "Initialize sortInsert before calling push" << std::endl;
-		return;
-	}
 	node* n = new node(data);
 	/*if the list is empty pointer the head pointer to 
 	 newly created node */
@@ -61,10 +35,15 @@ void LinkedList<T>::push(const T& data){
 		head_ = n; 
 		return;
 	}
+
+	if(head_ != nullptr && sortInsertRev){
+		std::cerr << "Cannot push into list with change in order" << std::endl;	
+		return;
+	}
 	
 	node* current = head_;
 
-	if(reverse_){
+	if(!sortInsertRev){
 		if(data >= head_->data){
 			n->next = head_;
 			head_ = n;
@@ -102,7 +81,8 @@ void LinkedList<T>::display(){
 int main(){
 	LinkedList<int> l1, l2;
 	srand(time(0));
-	l1.sortedInsert(false);
+	l1.push(5);
+	l1.sortInsertRev = true;
 	for(int i=0; i<10; i++){
 			int t = rand()%100+1;
 			l1.push(t);
