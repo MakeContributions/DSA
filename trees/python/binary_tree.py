5cde215b461d235ef8106a8c1dfa9a342b4c88e0
Nirmal Karthikeyan <59725174+cyberwizard1001@users.noreply.github.com>
2021-04-16 15:44:36 +0530

    Update binary_tree.py

--- trees/python/binary_tree.py
+++ trees/python/binary_tree.py
@@ -52,10 +52,7 @@ class BinaryTree(object):
 tree = BinaryTree(1)
 tree.root.left = Node(2)
 tree.root.right = Node(3)
-tree.root.left.left = Node(4)
-tree.root.left.right = Node(5)
-tree.root.right.left = Node(6)
-tree.root.right.right = Node(7)
+
 
 tree.pre_order_traversal(tree.root)
 tree.in_order_traversal(tree.root)
