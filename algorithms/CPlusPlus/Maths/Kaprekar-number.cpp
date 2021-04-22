649070555c64fe9b3f7c68eff81f84acb13c03d8
Ujjwal <75884061+UG-SEP@users.noreply.github.com>
2021-04-23 01:26:41 +0530

    requested changes done

--- algorithms/CPlusPlus/Maths/Kaprekar-number.cpp
+++ algorithms/CPlusPlus/Maths/Kaprekar-number.cpp
@@ -3,60 +3,60 @@
 #include<iostream>
 class Kaprekar
 {
-    // num private member variable
-    int num;
+    // number private member variable
+    int number;
 public:
-    //constructor to initialize n in num
+    //constructor to initialize n in number
     Kaprekar(int n)
     {
-        num=n;
+        number=n;
     }
 
 // Function to calculate the total number of digits in the number.
 int total_digits()
 {
-    int t=0,s=num;
-    //until the s is not equal to 0 run the loop
-    while(s)
+    int total_count=0,copy=number;
+    //until the copy is not equal to 0 run the loop
+    while(copy)
     {
-        //divide s by 10 and assign it to s
-        s=s/10;
-        //increment t by 1
-        t++;
+        //divide copy by 10 and assign it to copy
+        copy=copy/10;
+        //increment total_count by 1
+        total_count++;
     }
-return t;
+return total_count;
 }
 //Function to check whether the number is Kaprekar or not.
-int is_kaprekar(int total,int n)
+int is_kaprekar(int total_count,int square,int number)
 {
-int i=0,arr[total],a=0,b=0,s=num;
-//until s is not equal to 0 run loop
-while(s!=0)
+int i=0,arr[total_count],left=0,right=0,copy=square;
+//until copy is not equal to 0 run loop
+while(copy!=0)
 {
-    // assign s%10 in arr[i]
- arr[i]=s%10;
-  //divide s by 10 and assign it to s
-  s=s/10;
+    // assign copy%10 in arr[i]
+ arr[i]=copy%10;
+  //divide copy by 10 and assign it to copy
+  copy=copy/10;
   //increment i by 1
   i++;
 }
-/* assign total digit in num variable in i and until i is greater and equal to
-total-(total/2) run the loop and decrement i by 1 */
-for(i=total-1;i>=total-(total/2);i--)
+/* assign total_count-1 in i and until i is greater and equal to
+total_count-(total_count/2) run the loop and decrement i by 1 */
+for(i=total_count-1;i>=total_count-(total_count/2);i--)
 {
-   //assign a*10+arr[i] in a
-   a=a*10+arr[i];
+   //assign left*10+arr[i] in left
+   left=left*10+arr[i];
 }
 //now until i is not equal to 0 run the loop
 while(i>=0)
 {
-// assign b*10+arr[i] in b
-b=b*10+arr[i];
+// assign right*10+arr[i] in right
+right=right*10+arr[i];
 // decrement i by 1
 i--;
 }
-// checking whether the number is kaprekar
-if(a+b==n)
+// checking whether the left+right sum is equal to number
+if(left+right==number)
     return 1;
 else
     return 0;
@@ -65,23 +65,23 @@ else
 //driver code
 int main(int argc,char *argv[])
 {
-    int n,total,res,s;
+    int number,total_count,result,square;
     std::cout<<"Enter a number: ";
-    //taking input in n
-    std::cin>>n;
-    s=n*n;
-    Kaprekar k(s);
-    //calling function k.total_digit() to get the total digit of number n
-    total=k.total_digits();
+    //taking input in number
+    std::cin>>number;
+    square=number*number;
+    Kaprekar k(square);
+    //calling function k.total_digit() to get the total digit of number
+    total_count=k.total_digits();
     //calling is_kaprekar function to check whether the number is kaprekar or not
-    res=k.is_kaprekar(total,n);
-    //printing the res which contain 1(kaprekar) or 0(not Kaprekar)
-    if(res)
-        std::cout<<n<<" is a kaprekar number"<< std::endl;
+    result=k.is_kaprekar(total_count,square,number);
+    //printing the result which contain 1(kaprekar) or 0(not Kaprekar)
+    if(result)
+        std::cout<<number<<" is a kaprekar number"<< std::endl;
     else
-        std::cout<<n<<" is not a kaprekar number"<< std::endl;
+        std::cout<<number<<" is not a kaprekar number"<< std::endl;
     return 0;
-	
+
 }
 /*
 Input: Enter a number: 9
