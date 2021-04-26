cb69a148df2267f0c860d209e01737c24f692970
divesharora <diveshngp@gmail.com>
2021-04-26 20:40:11 +0530

    lint support

--- algorithms/JavaScript/src/sorting/merge-sort.js
+++ algorithms/JavaScript/src/sorting/merge-sort.js
@@ -1,30 +1,30 @@
 function merge(left, right) {
-    let arr = []
+    let arr = [];
     // Break out of loop if any one of the array gets empty
     while (left.length && right.length) {
         // Pick the smaller among the smallest element of left and right sub arrays 
         if (left[0] < right[0]) {
-            arr.push(left.shift())  
+            arr.push(left.shift());
         } else {
-            arr.push(right.shift()) 
+            arr.push(right.shift());
         }
     }
     
     // Concatenating the leftover elements
     // (in case we didn't go through the entire left or right array)
-    return [ ...arr, ...left, ...right ]
+    return [ ...arr, ...left, ...right ];
 }
 
 function mergeSort(array) {
-  const half = array.length / 2
+  const half = array.length / 2;
   
   // Base case or terminating case
   if(array.length < 2){
-    return array 
+    return array;
   }
   
-  const left = array.splice(0, half)
-  return merge(mergeSort(left),mergeSort(array))
+  const left = array.splice(0, half);
+  return merge(mergeSort(left),mergeSort(array));
 }
 
 //testing
