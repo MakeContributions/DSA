0ac31fad4a6373c3e42506197c28120f822e5673
Ming Tsai <ming.jia1213@gmail.com>
2021-04-27 13:07:53 -0400

    chore: add on index.js

--- algorithms/JavaScript/src/index.js
+++ algorithms/JavaScript/src/index.js
@@ -12,6 +12,8 @@ require('./searching/linear-search');
 // Sorting
 require('./sorting/bubble-sort');
 require('./sorting/insertion-sort');
+require('./sorting/merge-sort');
+require('./sorting/quick-sort');
 require('./sorting/selection-sort');
 
 // Strings
