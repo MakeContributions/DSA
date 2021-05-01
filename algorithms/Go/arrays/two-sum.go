750f7c0c3a31c6cd57d03f451e993143884b91c8
Himanshu Ranjan <himanshuranjan@appointy.com>
2021-05-01 21:54:56 +0530

    add: two sum

---
+++ algorithms/Go/arrays/two-sum.go
@@ -0,0 +1,74 @@
+
+/*
+	Problem Statement : Given an array of integers nums and an integer target, return indices of
+						the two numbers such that they add up to target.
+						You may assume that each input would have exactly one solution, and you may
+						not use the same element twice.
+
+	Input: An array of integers and a target (int)
+	Output: array of indexes of len(2) with sum of element at that index equal to target or nil
+ */
+
+package arrays
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+
+/*
+	Using Brute Force : For every element check for another element if it exist in the array such that sum of
+						both the element is equals to the target
+	Time Complexity   : O(n^2)
+*/
+func twoSum(arr []int, target int) []int {
+
+	for i:= 0; i<len(arr); i++ {
+		for j:= i+1; j<len(arr); j++ {
+			if arr[i] + arr[j] == target {
+				return []int{i,j}
+			}
+		}
+	}
+	return nil
+}
+
+/*
+	Using Map := While traversing every element add the element as key and its position as its value in a map
+  				 Check the required value (i.e target - arr[i]) in the map
+  				 If the map contains the required value then we have two elements with the required sum and
+				 return the positions.
+	Time Complexity : O(n)
+ */
+func twoSumEfficient(arr []int, target int) []int {
+	m := make(map[int]int)
+	for i:=0; i<len(arr); i++ {
+
+		compliment := target - arr[i]
+		if _, ok := m[compliment]; ok {
+			return []int{m[compliment], i}
+		}
+		m[arr[i]] = i
+	}
+	return nil
+}
+
+func RunTwoSum() {
+
+	rand.Seed(time.Now().UnixNano())
+	arr := []int{2,7,11,15,1,13,18}
+	elem := rand.Intn(30)
+	fmt.Println("Target = ", elem)
+
+	// Using Brute Force
+	t := time.Now()
+	fmt.Println("Indexes: ", twoSum(arr, elem))
+	fmt.Println("Time Takes (in microseconds): ", time.Since(t).Microseconds())
+
+	// Using Efficient Solution (map)
+	t = time.Now()
+	fmt.Println("Indexes: ", twoSumEfficient(arr, elem))
+	fmt.Print("Time Takes (in microseconds): ", time.Since(t).Microseconds())
+}
