ce57381df747f8673bff7d2c65ea7ae73afd4418
Arpitgoyal <arpitgoyal0927@gmail.com>
2021-05-12 08:42:31 +0530

    linear search

--- algorithms/C/searching/linearsearch.c
+++ algorithms/C/searching/linearsearch.c
@@ -1,24 +1,67 @@
+// linear search
 #include <stdio.h>
- int search(int arr[], int n, int x)
+#define SIZE 100  //change according to question
+
+//function prototype
+int linearSearch(int a[], int query, int size);
+
+int main()
 {
-    int i;
-    for (i = 0; i < n; i++)
-        if (arr[i] == x)
-            return i;
-    return -1;
+   int data[ SIZE ], i, temp;
+   int searchQuery; //value to search in the array
+
+   //generate some data
+   for (i = 0; i < SIZE; ++i)
+   {
+      data[ i ] = 2 * i;
+   }
+
+   printf("Enter integer to search for: ");
+   scanf("%d", &searchQuery);
+
+   //finding searchQuery in array data
+   temp = linearSearch(data, searchQuery, SIZE);
+
+   //condition for displaying result
+   if ( temp != -1)
+      printf("Found value in element %d.", temp);
+   else
+      printf("Value not found.");
+
+   return 0;
 }
- 
-// Driver code
-int main(void)
+
+//Linear search function
+
+int linearSearch(int a[], int query, int size)
 {
-    int arr[] = { 2, 3, 4, 10, 40 };
-    int x = 10;
-    int n = sizeof(arr) / sizeof(arr[0]);
-   
-    // Function call
-    int result = search(arr, n, x);
-    (result == -1)
-        ? printf("Element is not present in array")
-        : printf("Element is present at index %d", result);
-    return 0;
+   int j; //counter variable
+
+   //loop through array
+   for (j = 0; j < size; ++j)
+   {
+      if ( a[ j ] == query)
+      {
+         return j; //return location of value
+      }
+   }
+
+   return -1; //value not found
 }
+/*
+Case 1:
+Input: Enter size: 10
+       1,2,3,4,5,6,7,8,9,10
+       Enter the data to search: 11
+Output: Data not Found
+
+Case 2:
+
+Input: Enter size: 10
+       1,2,3,4,5,6,7,8,9,10
+       Enter the data to search: 7
+Output: Data Found at index: 6
+
+Time complexity: O(n)  //where n is the size of array
+
+*/
