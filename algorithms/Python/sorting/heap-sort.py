567c21cdb2023c29d93e6cc85c377d0e8e51663a
Ranu Singh <sranu2109@gmail.com>
2021-05-14 15:32:27 +0530

    Update heap-sort.py

--- algorithms/Python/sorting/heap-sort.py
+++ algorithms/Python/sorting/heap-sort.py
@@ -5,23 +5,23 @@ def heapsort(alist):
     for i in range(len(alist) - 1, 0, -1):
         alist[0], alist[i] = alist[i], alist[0]
         max_heapify(alist, index=0, size=i)
- 
+
 def parent(i):
     return (i - 1)//2
- 
+
 def left(i):
     return 2*i + 1
- 
+
 def right(i):
     return 2*i + 2
- 
+
 def build_max_heap(alist):
     length = len(alist)
     start = parent(length - 1)
     while start >= 0:
         max_heapify(alist, index=start, size=length)
         start = start - 1
- 
+
 def max_heapify(alist, index, size):
     l = left(index)
     r = right(index)
@@ -34,10 +34,11 @@ def max_heapify(alist, index, size):
     if (largest != index):
         alist[largest], alist[index] = alist[index], alist[largest]
         max_heapify(alist, largest, size)
- 
- 
+
+
 alist = input('Enter the list of numbers: ').split()
-alist = [int(x) for x in alist]
+alist = [x for x in alist] 
+alist = list(map(int, alist))
 heapsort(alist)
 print('Sorted list: ', end='')
 print(alist)
@@ -49,7 +50,6 @@ Test case 1.
 Enter the list of numbers: -1
 Sorted list: [-1]
 Time complexity : Best case = Avg case = Worst case = O(n logn)
-
 Test case 2.
 Enter the list of numbers: 10  5  0  -3  -1
 Sorted list: [-3  -1  0  5  10]
