b8d8a50382f698e2e03090908d15e7a94f10e1a1
Aayush Jain <jaayush014@gmail.com>
2021-05-21 12:02:57 +0530

    space and indentation fix

--- algorithms/Java/trees/zig-zag-traversal.java
+++ algorithms/Java/trees/zig-zag-traversal.java
@@ -4,75 +4,59 @@ import java.util.*;
 import java.util.HashMap;
 import java.io.*;
 
-    class Node{
-        int data;
-        Node left,right;
-        Node(int data){
-            this.data = data;
-            left=null;
-            right=null;
-        }
+class Node{
+    int data;
+    Node left,right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
     }
+}
 
 //Main Class
 public class Main {
     static Node buildTree(String str){
-        
         if(str.length()==0 || str.charAt(0)=='N'){
             return null;
         }
-        
         String ip[] = str.split(" ");
         // Create the root of the tree
         Node root = new Node(Integer.parseInt(ip[0]));
         // Push the root to the queue
-        
         Queue<Node> queue = new LinkedList<>(); 
-        
         queue.add(root);
         // Starting from the second element
-        
         int i = 1;
         while(queue.size()>0 && i < ip.length) {
-            
             // Get and remove the front of the queue
             Node currNode = queue.peek();
             queue.remove();
-                
             // Get the current node's value from the string
             String currVal = ip[i];
-                
             // If the left child is not null
             if(!currVal.equals("N")) {
-                    
                 // Create the left child for the current node
                 currNode.left = new Node(Integer.parseInt(currVal));
                 // Push it to the queue
                 queue.add(currNode.left);
-            }
-                
+            
             // For the right child
             i++;
             if(i >= ip.length)
-                break;
-                
+                break;    
             currVal = ip[i];
-                
             // If the right child is not null
             if(!currVal.equals("N")) {
-                    
                 // Create the right child for the current node
                 currNode.right = new Node(Integer.parseInt(currVal));
-                    
                 // Push it to the queue
                 queue.add(currNode.right);
             }
             i++;
         }
-        
         return root;
     }
-    
 	public static void inorder(Node root) {
 	    if(root==null)
 	    return;
@@ -80,33 +64,25 @@ public class Main {
 	    System.out.print(root.data);
 	    inorder(root.right);
 	}
-     /* Drier program to test above functions */
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        
-	        int t=Integer.parseInt(br.readLine());
-    
-	        while(t > 0){
-	            String s = br.readLine();
-    	    	Node root = buildTree(s);
-        	    GFG g = new GFG();
-			
-			    ArrayList<Integer> res = g.zigZagTraversal(root) ;
-			    for (int i = 0; i < res.size (); i++)
-			        System.out.print (res.get (i) + " ");
-			    System. out. println();  
-    			
-                t--;
-            
+        int t=Integer.parseInt(br.readLine());
+        while(t > 0){
+            String s = br.readLine();
+            Node root = buildTree(s);
+            GFG g = new GFG();
+            ArrayList<Integer> res = g.zigZagTraversal(root) ;
+            for (int i = 0; i < res.size (); i++)
+                System.out.print (res.get (i) + " ");
+            System. out. println();  
+            t--;
         }
 	}
 }
 // Print the zig zag traversal of tree.
-class GFG
-{
+class GFG {
     // return an array containing the zig zag level order traversal of the given tree
-	ArrayList<Integer> zigZagTraversal(Node root)
-	{
+	ArrayList<Integer> zigZagTraversal(Node root) {
 	    ArrayList<Integer> al = new ArrayList<>();
         // if null then return empty list
         if (rootNode == null)
