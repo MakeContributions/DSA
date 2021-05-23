e19ad2dc09897b9a590c1948fa194df325170319
Shivam Gupta <skg050210@gmail.com>
2021-05-23 12:16:09 +0530

    Update bubble-sort.java

--- algorithms/Java/sorting/bubble-sort.java
+++ algorithms/Java/sorting/bubble-sort.java
@@ -1,17 +1,16 @@
-package JAVA;
 import  java.util.Scanner;
 public class sorting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        boolean sorted = true;
+        boolean sorted = true; 
 
         System.out.println("Enter the size of array: ");
         int n=sc.nextInt();
 
         int a[]=new int[n];
 
-        System.out.println("Enter the elements of array : ");
+        System.out.println("Enter the elements of array : "); 
         for (int i=0;i<n;i++)
         {
             a[i]=sc.nextInt();
