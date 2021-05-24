505cb84bd2bbd2faae08a499fe642f4cf8c3d5ae
Ashborn-SM <rahulpillai0128@gmail.com>
2021-05-24 22:49:17 +0530

    Fix bugs

--- algorithms/CPlusPlus/Trees/avl.cpp
+++ algorithms/CPlusPlus/Trees/avl.cpp
@@ -4,25 +4,41 @@ struct TreeNode{
 	TreeNode* left;
 	TreeNode* right;
 	int data, height;
-	TreeNode(const int& data): data(data), left(nullptr), right(nullptr), height(-1){}
+	TreeNode(const int& data): data(data), left(nullptr), right(nullptr), height(0){}
 };
  
 int height(TreeNode* node){
+	/**
+	 * Return the height of the node
+	 */
+
 	if(node == nullptr){ return -1; }
 	return node->height;
 }
 
 void updateheight(TreeNode*& node){
+	/**
+	 * Update the height of the node
+	 */
+
 	if(node == nullptr){ return; }
 	node->height = 1 + std::max(height(node->left), height(node->right));
 }
 
 int balance_factor(TreeNode* node){
-	if(root != nullptr){ return height(node->right) - height(node->left); }
+	/**
+	 * Find the balance factor of the given node
+	 */
+
+	if(node != nullptr){ return height(node->right) - height(node->left); }
 	return 0;
 }
 
 void leftrotation(TreeNode*& node){
+	/**
+	 * Rotate the sub-tree rooted with the node to left
+	 */
+
 	if(node == nullptr){ return; }
 
 	TreeNode* a = node;
@@ -37,6 +53,10 @@ void leftrotation(TreeNode*& node){
 }
 
 void rightrotation(TreeNode*& node){
+	/**
+	 * Rotate the sub-tree rooted with the node to right
+	 */
+
 	if(node == nullptr){ return; }
 
 	TreeNode* a = node;
@@ -46,11 +66,40 @@ void rightrotation(TreeNode*& node){
 	b->right= a;
 	node = b;
 
-	updateheight(a);
+	updateheight(a); 
 	updateheight(b);
 }
 
+void right_leftrotation(TreeNode*& node){
+	/**
+	 * Rotate the sub-tree rooted with the node to the right and then left
+	 */
+
+	if(node == nullptr){ return; }
+	
+	rightrotation(node->right);
+	leftrotation(node);
+}
+
+void left_rightrotation(TreeNode*& node){
+	/**
+	 * Rotate the sub-tree rooted with the node to the left and then right  
+	 */
+
+	if(node == nullptr){ return; }
+	
+	leftrotation(node->left);
+	rightrotation(node);
+}
+
 void insert(TreeNode*& root, const int& data){
+	/**
+	 * Create and insert the node in the appropriate place of the tree
+	 * Update the height of every node in the tree
+	 * Check the balance factor of the node and do the appropriate rotation
+	 * to make the tree balanced
+	 */
+
 	if(root == nullptr) { 
 		root = new TreeNode(data);
 	}
@@ -61,53 +110,78 @@ void insert(TreeNode*& root, const int& data){
 
 	const int initial_bal = balance_factor(root) ; 
 
-	if(initial_bal > 2 || initial_bal < -2){ throw std::runtime_error(" Error "); }
-
 	if(initial_bal == 2){
-		l_bal = balance_factor(root->left);
-		if(l_bal == 1 || l_bal == 0){ leftrotation(root); }	
-		else if(l_bal == -1){ right_leftrotation(root); }
+		int r_bal = balance_factor(root->right);
+		if(r_bal == 1 || r_bal == 0){ leftrotation(root); }	
+		else if(r_bal == -1){ right_leftrotation(root); }
 	}
 	else if(initial_bal == -2){
-		r_bal = balance_factor(root->right); 
-		if(r_bal == 1 || r_bal == 0){ left_rigtrotation(root); } 
-		else if(r_bal == -1){ rightrotation(root); }
+		int l_bal = balance_factor(root->left); 
+		if(l_bal == 1 || l_bal == 0){ left_rightrotation(root); } 
+		else if(l_bal == -1){ rightrotation(root); }
 	}
-}
-
-TreeNode* find(TreeNode* root, const int& data){
-	if(root == nullptr){ throw std::runtime_error("Item not found"); }
-	else if(root->data == data){ return root; }
-	else if(root->data < data){ return find(root->right, data); }
-	else{ return find(root->left, data); }
-}
-
-void ensureBalance(TreeNode* node){
-	if(node == nullptr){ return; }
-
 }
 
 void print(TreeNode* root){
 	if(root != nullptr){
-		print(root->left);
 		std::cout << root->data << " "; 
+		print(root->left);
 		print(root->right);
 	}
 }
 
 int main(){
 	TreeNode* root = nullptr;
-	insert(root, 37);
-        insert(root, 19);
-        insert(root, 4);
-        insert(root, 22);
-        insert(root, 51);
-        insert(root, 55);
-        insert(root, 42);
-        insert(root, 20);
-        insert(root, 11);
+	insert(root, 1);
         insert(root, 2);
+        insert(root, 3);
+        insert(root, 4);
+        insert(root, 5);
+        insert(root, 6);
+        insert(root, 7);
+        insert(root, 8);
+        insert(root, 9);
+        insert(root, 10);
+
+	/*
+
+      Binary tree                         
+	   1  
+	    \
+	     2
+	      \
+	       3
+	        \
+		 4
+		  \
+		   5
+		    \
+		     6
+		      \
+		       7
+		        \
+			 8
+			  \
+			   9
+			    \
+			     10
+	
+	Output: 1 2 3 4 5 6 7 8 9 10
+	Here the balance factor of the root node is 9                          
+
+	AVL Tree
+	   4
+	 /  \
+	2    8
+       /\   / \
+      1  3 6   9
+	  / \   \
+	 5   7	 10
+
+	Output: 4 2 1 3 8 6 5 7 9 10
+	Here the balance factor of the root node is 1
+
+	*/
 
 	print(root);
-
 }
