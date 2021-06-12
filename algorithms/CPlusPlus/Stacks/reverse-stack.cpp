2880162e30edb0be52c972a8158e98023bf0c641
anirudh09041 <anirudh09041@gmail.com>
2021-06-12 19:59:23 +0530

    description added

--- algorithms/CPlusPlus/Stacks/reverse-stack.cpp
+++ algorithms/CPlusPlus/Stacks/reverse-stack.cpp
@@ -1,3 +1,6 @@
+/* the idea is to hold all items in a call stack until the stack becomes empty.
+Then insert each item in the call stack at the bottom of the stack. */
+
 #include <iostream>
 #include <stack>
 using namespace std;
@@ -16,8 +19,8 @@ void insertInBottom(stack<int> &s, int item)
     s.pop();
     insertInBottom(s, item);
 
-    // After the recursion completes , push each item in the call stack
-    // to top of the stack
+    /*After the recursion completes , push each item in the call stack
+     to top of the stack*/
     s.push(top);
 }
 
@@ -34,16 +37,19 @@ void reverseStack(stack<int> &s)
     s.pop();
     reverseStack(s);
 
-    // After the recursion unfolds, insert each item from the call stack
-    // to bottom of the stack
+    // After the recursion unfolds, insert each item from the call stack to bottom of the stack
     insertInBottom(s, item);
 }
 
 int main()
 {
     stack<int> s;
-    for (int i = 1; i <= 5; i++) {
+    int n;
+    cout<<"Enter the size of stack"<<"\n";
+    cin>>n;
+    for (int i = 1; i <= n; i++) {
         int temp;
+        cout<<"enter element"<<"\n";
         cin>>temp;
         s.push(temp);
     }
@@ -55,6 +61,13 @@ int main()
         cout << s.top() << ' ';
         s.pop();
     }
+    
+    /*what program does (reverses the stack by using recursion)
+    -> original stack: 1,2,3,4,5
+    -> reversed stack: 5,4,3,2,1*/
+    
+    // time complexity : O(n^2)
+
 
     return 0;
 }
