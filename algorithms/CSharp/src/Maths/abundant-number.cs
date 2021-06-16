548b9fc22e4570d8392ae3d512ce881f222fcfdf
Ujjwal <75884061+UG-SEP@users.noreply.github.com>
2021-06-16 21:49:52 +0530

    refactor abundant number

--- algorithms/CSharp/src/Maths/abundant-number.cs
+++ algorithms/CSharp/src/Maths/abundant-number.cs
@@ -1,28 +1,32 @@
 /* Program to check whether a number is abundant or not */
 using System;
 
-namespace abundant_number
+namespace Abundant.Number
 {
-    class Solution
-    { 
-        // function to check whether number is abundant or not
-        public static bool Abudant(int num)
+    class Abundant
     {
+        // function to check whether number is abundant or not
+        public static bool IsAbudant(int num)
+          {
             int i, sum = 0;
             // loop run until it is less than or equal to half of the number
             for (i = 1; i <= num / 2; i++)
-            {
+             {
                 // if the remainder becomes 0 add that number to sum
                 if (num % i == 0)
                     sum += i;
-            }
+             }
             // if the sum is greater than num that means it is an abundant number
             if (sum > num)
+             {
                 return true;
-            // it is not an abundant number
-            else
+             }
+            else // it is not an abundant number
+             {
                 return false;
-        }
+             }
+          }
+
         // driver code
         static void Main(string[] args)
         {
@@ -31,13 +35,16 @@ namespace abundant_number
             Console.WriteLine("Enter a number: ");
             // reading data and converting into into int to store that in num variable
             num = Convert.ToInt32(Console.ReadLine());
-            res = Abudant(num);
+            res = IsAbudant(num);
             if (res)
+            {
                 Console.WriteLine($"{num} is an abundant number");
+            }
             else
+            {
                 Console.WriteLine($"{num} is not an abundant number");
+            }
         }
-
     }
 }
 /*
