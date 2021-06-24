abaa33edbd876cb89f00b962ada6f716b32a7013
Jesus Fidel Fraile Gonzalez <jesusfidelfraile@hotmail.com>
2021-06-24 15:19:54 +0200

    Added time complexity Refactored typos Fixed bug in Fibonacci::printFibonacci_R. Now it shows the numbers properly

--- algorithms/CPlusPlus/Maths/fibonacci-series.cpp
+++ algorithms/CPlusPlus/Maths/fibonacci-series.cpp
@@ -1,9 +1,8 @@
-
-
 #include <iostream>
 
 using namespace std;
 
+class Fibonacci {
 /**
  *   The Fibonacci Sequence is the series of numbers:
  *
@@ -11,19 +10,20 @@ using namespace std;
  *
  *   The next number is found by adding up the two numbers before it:, next number is the sum of previous two numbers
  *   for example 0,
- *   1, 1, 2, 3, 5, 8, 13, 21 etc. The first two numbers of fibonacci series are 0 and 1.
+ *   1, 1, 2, 3, 5, 8, 13, 21 etc. 
+ *   The first two numbers of fibonacci series are 0 and 1.
+ *   Time complexity: O(n)
  *
  * */
-
-class Fibonaccci {
 public:
-
-    void print_fibonaccci_no_R() {
+    /*
+        Example 1: the number of elements is introduced by the user via command promnt.
+    */
+    void print_fibonacci_no_R() {
         int num1 = 0, num2 = 1, num3, i, number;
         cout << "Enter the number of elements: ";
         cin >> number;
         cout << num1 << " " << num2 << " "; //printing 0 and 1
-
 
         for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed
         {
@@ -32,29 +32,34 @@ public:
             num1 = num2;
             num2 = num3;
         }
+        cout << "\n\n"; //printing 0 and 1
     }
 
+    /*
+    Example 2: the number of elements is introduced by the user via command promnt.
+    */
     void printFibonacci_R(int n) {
-        if (n > 0) {
+        if (n > 2) { // n finishes at 2 because 0 and 1 are already printed
+            if (n1 == 0) cout << n1 << " " << n2 << " "; //printing 0 and 1
             n3 = n1 + n2;
             n1 = n2;
             n2 = n3;
             cout << n3 << " ";
             printFibonacci_R(n - 1);
         }
-    };
+    }
 
 private:
-    int n1 {0} ;
-    int n2 {1} ;
-    int n3  ;
-    
+    int n1{0};
+    int n2{1};
+    int n3;
+
 };
 
 
 int main() {
-    Fibonaccci f;
-    // f.print_fibonaccci_no_R();
+    Fibonacci f;
+    f.print_fibonacci_no_R();
     f.printFibonacci_R(8);
     return 0;
 }
