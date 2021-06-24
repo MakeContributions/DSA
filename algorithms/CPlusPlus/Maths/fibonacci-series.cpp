ec5c0fd953c41b17533e9239df5c9dcee6c7b200
Jesus Fidel Fraile Gonzalez <jesusfidelfraile@hotmail.com>
2021-06-24 17:03:38 +0200

    Resolved faulty description in example 2 comment

--- algorithms/CPlusPlus/Maths/fibonacci-series.cpp
+++ algorithms/CPlusPlus/Maths/fibonacci-series.cpp
@@ -36,7 +36,7 @@ public:
     }
 
     /*
-    Example 2: the number of elements is introduced by the user via command prompt.
+    Example 2: the number of elements is introduced by the user via function input parameter.
     */
     void printFibonacci_R(int n) {
         if (n > 2) { // n finishes at 2 because 0 and 1 are already printed
@@ -63,6 +63,3 @@ int main() {
     f.printFibonacci_R(8);
     return 0;
 }
-
-
-
