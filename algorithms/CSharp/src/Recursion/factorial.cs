61dc78d2d3a89ced9bbb4bba18b259db0e845a73
Waqar-107 <waqar.hassan866@gmail.com>
2021-08-30 01:45:16 +0600

    added c# code to determine factorial

---
+++ algorithms/CSharp/src/Recursion/factorial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Algorithms.Recursion
+{
+    public class Factorial
+    {
+        public static int FactorialUsingRecursion(int number)
+        {
+            if(number == 0 || number == 1)
+            {
+                return 1;
+            }
+
+            return number * FactorialUsingRecursion(number - 1);
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine($"Factorial of 7 is: {FactorialUsingRecursion(17)}");
+        }
+    }
+}
+
+/*
+ * n! = n * (n - 1) * (n - 2) * .... * 3 * 2 * 1
+ * we have used int here but for larger number, int will not be enough
+ */
