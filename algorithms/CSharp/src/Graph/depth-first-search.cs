817b846fb5266b707c71d04d7b03fac8d71c4b6a
Waqar-107 <waqar.hassan866@gmail.com>
2021-09-04 02:38:25 +0600

    added cycle detection with unit tests

--- algorithms/CSharp/src/Graph/depth-first-search.cs
+++ algorithms/CSharp/src/Graph/depth-first-search.cs
@@ -9,14 +9,14 @@ namespace Algorithms.Graph
         class Graph
         {
             private int _vertex;
-            private bool _isCycle;
+            private bool _cycleFound;
             private List<List<int>> _adjacencyList;
             private List<int> _parent;
             private List<int> _visited;
             private List<int> _traversal;
 
             public const int WHITE = 0;
-            public const int BROWN = 1;
+            public const int GREY = 1;
             public const int BLACK = 2;
 
             public Graph(int vertex)
@@ -46,9 +46,20 @@ namespace Algorithms.Graph
                 return _traversal;
             }
 
+
+            public bool HasCycle()
+            {
+                _parent = Enumerable.Repeat(-1, _vertex + 1).ToList();
+                _visited = Enumerable.Repeat(WHITE, _vertex + 1).ToList();
+                _traversal.Clear();
+
+                DFSRecursive(1);
+                return _cycleFound;
+            }
+
             private void DFSRecursive(int source)
             {
-                _visited[source] = BROWN;
+                _visited[source] = GREY;
                 _traversal.Add(source);
 
                 foreach (int destination in _adjacencyList[source])
@@ -58,17 +69,23 @@ namespace Algorithms.Graph
                         _parent[destination] = source;
                         DFSRecursive(destination);
                     }
+
+                    else if (_visited[destination] == GREY && destination != _parent[source])
+                    {
+                        _cycleFound = true;
+                    }
                 }
 
                 _visited[source] = BLACK;
             }
+
         }
 
 
         public static List<int> DFS(int vertex, int src, List<Tuple<int, int>> edges)
         {
             Graph graph = new Graph(vertex);
-            foreach(var edge in edges)
+            foreach (var edge in edges)
             {
                 graph.AddEdge(edge.Item1, edge.Item2);
                 graph.AddEdge(edge.Item2, edge.Item1);
@@ -77,20 +94,38 @@ namespace Algorithms.Graph
             return graph.DFS(src);
         }
 
+        public static bool HasCycle(int vertex, List<Tuple<int, int>> edges)
+        {
+            Graph graph = new Graph(vertex);
+            foreach (var edge in edges)
+            {
+                graph.AddEdge(edge.Item1, edge.Item2);
+                graph.AddEdge(edge.Item2, edge.Item1);
+            }
+
+            return graph.HasCycle();
+        }
+
         public static void Main()
         {
             List<Tuple<int, int>> edges = new List<Tuple<int, int>>();
             edges.Add(new Tuple<int, int>(1, 2));
             edges.Add(new Tuple<int, int>(1, 3));
-            edges.Add(new Tuple<int, int>(2, 3));
-            edges.Add(new Tuple<int, int>(4, 2));
-            edges.Add(new Tuple<int, int>(1, 5));
+            edges.Add(new Tuple<int, int>(4, 3));
+            edges.Add(new Tuple<int, int>(3, 5));
 
-            List<int> traversal = DFS(5, 3, edges);
-            foreach(int v in traversal)
+            List<int> traversal = DFS(5, 1, edges);
+            foreach (int v in traversal)
             {
                 Console.Write($"{v} ");
             }
+
+            Console.WriteLine($"Has Cycle: {HasCycle(5, edges)}");
         }
     }
 }
+
+/*
+ * depth first search for an undirected graph
+ * also cycles are detected
+ */
--- algorithms/CSharp/test/Graph/depth-first-search.cs
+++ algorithms/CSharp/test/Graph/depth-first-search.cs
@@ -7,7 +7,7 @@ namespace Algorithms.Tests.Graph
     [TestFixture]
     public class DepthFirstSearch
     {
-        static object[] DivideCases =
+        static object[] DivideCasesForDFS =
         {
             new object[] { 5, 3, new List<Tuple<int, int>> {
                                  new Tuple<int, int>(1, 2),
@@ -26,12 +26,39 @@ namespace Algorithms.Tests.Graph
             }
         };
 
+        static object[] DivideCasesForCycleDetection =
+        {
+            new object[] { 5, new List<Tuple<int, int>> {
+                                new Tuple<int, int>(1, 2),
+                                new Tuple<int, int>(1, 3),
+                                new Tuple<int, int>(2, 3),
+                                new Tuple<int, int>(4, 2),
+                                new Tuple<int, int>(1, 5)
+                         }, true
+            },
+            new object[] { 5, new List<Tuple<int, int>> {
+                                new Tuple<int, int>(1, 2),
+                                new Tuple<int, int>(1, 3),
+                                new Tuple<int, int>(4, 3),
+                                new Tuple<int, int>(3, 5)
+                         }, false
+            }
+        };
 
-        [TestCaseSource(nameof(DivideCases))]
+
+        [TestCaseSource(nameof(DivideCasesForDFS))]
         public void TestDFS_ShouldGetExpectedResult(int vertex, int source, List<Tuple<int, int>> edges, string expected)
         {
             List<int> result = Algorithms.Graph.DepthFirstSearch.DFS(vertex, source, edges);
             Assert.AreEqual(expected, string.Join(' ', result));
+        }
+
+
+        [TestCaseSource(nameof(DivideCasesForCycleDetection))]
+        public void TestCycleDetection_ShouldGetExpectedResult(int vertex, List<Tuple<int, int>> edges, bool expected)
+        {
+            bool result = Algorithms.Graph.DepthFirstSearch.HasCycle(vertex, edges);
+            Assert.AreEqual(expected, result);
         }
     }
 }
