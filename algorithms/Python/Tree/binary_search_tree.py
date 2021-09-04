45695dd382d3f867edcb208df7b6d4810d4463e4
Arsenic <54987647+Arsenic-ATG@users.noreply.github.com>
2021-09-04 22:35:11 +0530

    Fix BST.py to pass python test

--- algorithms/Python/Tree/binary_search_tree.py
+++ algorithms/Python/Tree/binary_search_tree.py
@@ -1,13 +1,14 @@
-
 # Binary Tree Data Structure
 #     Data is stored in hierarchical form where a parent node can have at most 2 child nodes
-#                 A
-#      
-#            B       C
-#         
-#        D       E
-#      
-#     F    G       H
+#
+#                  A
+#               ___|___
+#              B       C
+#           ___|___
+#          D       E
+#        /  \       \
+#       F    G       H
+#
 #         Here, A is "ROOT NODE" and B, C are "CHILD NODE"
 #            (B-D-F-G), (B-E-H) is a sub-tree
 #            B is 'ROOT NODE' for D, E & D is 'ROOT NODE' for F, G & E is root node for H
