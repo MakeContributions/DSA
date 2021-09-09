e7503a6e5dd27a931d644292d44327273eb7b450
Subham <subhampandeywbjee12@gmail.com>
2021-09-09 14:12:13 +0530

    changes updated

--- algorithms/CPlusPlus/Linked-Lists/remove-duplicates-in-sorted-linked-list.cpp
+++ algorithms/CPlusPlus/Linked-Lists/remove-duplicates-in-sorted-linked-list.cpp
@@ -40,6 +40,7 @@ void removeduplicates(Node *head){
         if(curr->data==curr->next->data){
             Node *temp=curr->next;
             curr->next=curr->next->next;
+            delete temp;
         }
         else{
             curr=curr->next;
--- algorithms/CPlusPlus/Linked-Lists/sadkjasdkajsdk.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-#include <array>
-using namespace std;
-int main(){
-	std:: array<int, 5> myarray={1,2,3,4,5};
-	for(int i=0;i<array.size();i++){
-		cout<<myarray[i];
-	}
-}
