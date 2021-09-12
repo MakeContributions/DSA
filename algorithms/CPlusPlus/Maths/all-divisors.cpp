5028e0c0d96bb260e1f9a51edb136f886140660c
Khushi <khushisinha4786@gmail.com>
2021-09-12 19:30:02 +0530

    Added

--- algorithms/CPlusPlus/Maths/all-divisors.cpp
+++ algorithms/CPlusPlus/Maths/all-divisors.cpp
@@ -1,3 +1,6 @@
+//This program takes a positive integer as an input and prints all the divisors of that integer.
+//The code makes use of the fact that all divisors exist in pairs.
+
 #include <bits/stdc++.h> 
 using namespace std;
 
@@ -18,3 +21,9 @@ int main() {
 	printDivisors(n);
 	return 0;
 }
+//Time Complexity: O(sqrt(n))
+//Auxiliary Space: O(1)
+
+//Example Test Case:
+//Input: 10
+//Output: 1 2 5 10
