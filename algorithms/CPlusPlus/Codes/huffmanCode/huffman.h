ff72602e74fff0d99ab5d14c5deea18562ee25fb
Kareim Tarek AbdelAzeem <kareimgazar1990@gmail.com>
2021-09-16 02:27:46 +0200

    added Huffman Code algorithm
    Signed-off-by: Kareim Tarek AbdelAzeem <kareimgazar1990@gmail.com>

---
+++ algorithms/CPlusPlus/Codes/huffmanCode/huffman.h
@@ -0,0 +1,33 @@
+#include <string>
+using namespace std;
+
+class Node{
+public:
+    Node * parent;
+    Node * left;
+    Node * right;
+    char key;
+    int freq;
+    string name;
+    Node(Node * p, Node * l, Node * r, int k, int f)
+    {
+        parent = p;
+        left = l;
+        right = r;
+        key = k;
+        freq = f;
+    }
+};
+
+// reduces file size
+void minifyFile(string filePath, string modifiedSourceFilePath);
+
+// tree of ascii chars, used as the codes references
+Node * buildCodeTree(string alphabetSourceFilePath);
+
+// linesNum tells how many binary lines to be converted
+int compresstoBinaryFile(string modifiedSourceFilePath, Node * alphabetRoot);
+
+// decompresses the binary file into text file
+void decompressBinaryFile(string binarySourceFilePath, string textDistnationFilePath,int linesNum, Node * alphabetRoot);
+
