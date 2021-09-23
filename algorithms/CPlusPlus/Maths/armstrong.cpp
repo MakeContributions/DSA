d7077ce31405d3c77a3e0824025eb40af126a875
saurabh <kumarsyadav2@gmail.com>
2021-09-23 15:21:10 +0530

    This armstrong code is working right now

--- algorithms/CPlusPlus/Maths/armstrong.cpp
+++ algorithms/CPlusPlus/Maths/armstrong.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
-<<<<<<< HEAD
-int
-main ()
+int main ()
 {
   int a = 0, t, n, s, r, p, i, sum = 0;
   cin >> n;
@@ -29,22 +27,4 @@ main ()
   else
     cout << n << " is not an armstrong number.";
   return 0;
-=======
-int main() {
-    int num, i=0,r,sum=0,s;
-    //cout <<"enter a number to check it is Armstrong no. or not ";
-    cin>>num; 
-    s=num;
-    while (i<num)
-    {
-        r=num%10;
-        sum =sum+(r*r*r);
-        num =num/10;
-    }
-    if(sum==s)
-    cout <<"it is a Armstrong number ";
-    else 
-    cout <<"it is not a Armstrong number ";
-    return 1;
->>>>>>> 463635d6858dc412e2bcaa4a2c7a19d95ab2677a
 }
