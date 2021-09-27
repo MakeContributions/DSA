2ed607f373eca84db0873c7bc99d2fa4987e2773
samughodake <samughodake1808@gmail.com>
2021-09-27 10:22:05 +0530

    made required changes

--- algorithms/CPlusPlus/Arrays/segregate0and1.cpp
+++ algorithms/CPlusPlus/Arrays/segregate0and1.cpp
@@ -1,48 +1,53 @@
 /*
 Description: A program to segregate the array of 0s and 1s
 
-Approach: Counting the number of 0s present in the array and saving it in variable z. For the first z indexes, putting 0 and for the remaining indexes puting 1.
+Approach: Counting the number of 0s present in the array and 
+saving it in variable z. For the first z indexes, putting 0 and 
+for the remaining indexes putting 1.
 
 Time complexity: O(n)
 */
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void segregate0and1(int arr[], int n) {
+void segregate0and1(vector<int> &v, int n) {
     
     int z=0;
     for(int i=0;i<n;i++){
       //counting the number of 0s and storing it the variable z
-      if(arr[i]==0){
+      if(v[i]==0){
         z++;
       }
     }
-    //for z indexes, putting 0 in the array
-    //for remaining indexes putting 1 in the array
+    //for z indexes, putting 0 in the vector
+    //for remaining indexes putting 1 in the vector
     for(int j=0;j<n;j++){
       if(j<z){
-        arr[j]=0;
+               v[j]=0;
       }
       else{
-        arr[j]=1;
+               v[j]=1;
       }
     }       
 }
 int main() {
-    cout << "Enter number of elements in array: \n";
     int n;
+    cout<<"Enter number of array elements\n";
     cin>>n;
-
+    vector<int> v(n);
     cout<<"Enter the array elements (only 0 and 1)\n";
-    int arr[n];
+   
     for(int i=0;i<n;i++){
-      cin>>arr[i];
+     cin>>v[i];
+     v.push_back(v[i]);
     }
     //calling the function
-    segregate0and1(arr,n);
+    segregate0and1(v,n);
+    
     cout<<"After segregating, the array is: \n";
     for(int j=0;j<n;j++){
-      cout<<arr[j]<<" ";
+      cout<<v[j]<<" ";
     }
 }
