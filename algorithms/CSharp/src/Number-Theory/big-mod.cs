b9569b26efa33abf8893b5a106fdcd7f72491436
Waqar-107 <waqar.hassan866@gmail.com>
2021-09-29 00:42:48 +0600

    added big mod algo

---
+++ algorithms/CSharp/src/Number-Theory/big-mod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Algorithms.NumberTheory
+{
+    public class BigMod
+    {
+        // (a ^ p) % m
+        public static long Mod(long a, long p, long m)
+        {
+            if(p == 0)
+            {
+                return 1;
+            }
+            if(p % 2 == 1)
+            {
+               return ((a % m) * Mod(a, p - 1, m)) % m;
+            }
+
+            long temp = Mod(a, p / 2, m);
+            return (temp * temp) % m;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine($"{Mod(4, 5, 3)}");
+        }
+    }
+}
