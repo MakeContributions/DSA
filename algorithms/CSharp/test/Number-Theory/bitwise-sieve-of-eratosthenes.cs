771416ed53c4ec51f648df07ba41d79328be11ba
Waqar-107 <waqar.hassan866@gmail.com>
2021-10-02 02:14:30 +0600

    code refactored

--- algorithms/CSharp/test/Number-Theory/bitwise-sieve-of-eratosthenes.cs
+++ algorithms/CSharp/test/Number-Theory/bitwise-sieve-of-eratosthenes.cs
@@ -9,7 +9,7 @@ namespace Algorithms.Tests.Number_Theory
         [TestCase(10, "2, 3, 5, 7")]
         [TestCase(25, "2, 3, 5, 7, 11, 13, 17, 19, 23")]
         [TestCase(100, "2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97")]
-        public void SieveOfEratosthenes_ShouldReturnExpected(int max, string expected)
+        public void BitwiseSieveOfEratosthenes_ShouldReturnExpected(int max, string expected)
         {
             List<int> primeNumbers = Algorithms.NumberTheory.BitwiseSieveOfEratosthenes.GeneratePrimeNumbers(max);
             Assert.AreEqual(expected, string.Join(", ", primeNumbers));
