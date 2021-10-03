1851e52b425ece7ef9bc6eabac73902759606cd9
Madhan Murugan <91775905+Capt-Madhan@users.noreply.github.com>
2021-10-03 20:46:16 +0530

    Update Longest_common_substring.java

--- algorithms/Java/strings/Longest_common_substring.java
+++ algorithms/Java/strings/Longest_common_substring.java
@@ -7,8 +7,7 @@ class GFG {
 	Returns length of longest common substring
 	of X[0..m-1] and Y[0..n-1]
 	*/
-	static int LCSubStr(char X[], char Y[],
-						int m, int n)
+	static int LCSubStr(char X[], char Y[], int m, int n)
 	{
 		// Create a table to store
 		// lengths of longest common
@@ -37,10 +36,8 @@ class GFG {
 					LCStuff[i][j] = 0;
 				else if (X[i - 1] == Y[j - 1])
 				{
-					LCStuff[i][j]
-						= LCStuff[i - 1][j - 1] + 1;
-					result = Integer.max(result,
-										LCStuff[i][j]);
+					LCStuff[i][j] = LCStuff[i - 1][j - 1] + 1;
+					result = Integer.max(result, LCStuff[i][j]);
 				}
 				else
 					LCStuff[i][j] = 0;
@@ -52,14 +49,20 @@ class GFG {
 	// Driver Code
 	public static void main(String[] args)
 	{
+		// Test 1:
 		String X = "abcdabaabbacd";
 		String Y = "abbabaabcabcd";
-
 		int m = X.length();
 		int n = Y.length();
-
-		System.out.println(LCSubStr(X.toCharArray(),
-									Y.toCharArray(), m,
-					n));
+		System.out.println(LCSubStr(X.toCharArray(), Y.toCharArray(), m, n)); //Output: 5 -> (The common subtring is 'abaab')
+		
+		// Test 2:
+		String X = "abcdefgh";
+		String Y = "abaabcfg";
+		int m = X.length();
+		int n = Y.length();
+		System.out.println(LCSubStr(X.toCharArray(), Y.toCharArray(), m, n)); //Output: 5 -> (The common subtring is 'abc')
 	}
 }
+// Time Complexity -> O(m*n)
+// where m and n are the lengths of the strings X and Y.
