5c602aab62beb25ffaf49c383a5d9a5dde88f484
Samruddhi Ghodake <72791227+samughodake@users.noreply.github.com>
2021-10-04 09:47:33 +0530

    Update algorithms/CPlusPlus/Arrays/search-insert-position.cpp
    Updated the comment in multiple lines
    
    Co-authored-by: Rahul Rajeev Pillai <66192267+Ashborn-SM@users.noreply.github.com>

--- algorithms/CPlusPlus/Arrays/search-insert-position.cpp
+++ algorithms/CPlusPlus/Arrays/search-insert-position.cpp
@@ -1,5 +1,7 @@
 /*
-Description: Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+Description: Given a sorted array of distinct integers and a target value, 
+return the index if the target is found. If not, return the index where it
+would be if it were inserted in order.
 
 Approach: Using Binary search to solve in least time complexity.
 
