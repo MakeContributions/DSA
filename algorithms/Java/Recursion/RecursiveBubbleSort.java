e40f33e75c1bb5273723bd745111817498267e08
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-10-07 19:12:56 +0530

    Add Recursion Problem

---
+++ algorithms/Java/Recursion/RecursiveBubbleSort.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        bubbleSort(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubbleSort(int[] arr, int n) {
+        if (n == 1) {
+            return;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr, i, i + 1);
+            }
+        }
+        bubbleSort(arr, n - 1);
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
