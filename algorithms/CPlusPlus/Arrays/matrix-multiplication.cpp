2428a9e93af2852a30ea7d6bf473c4052088a714
Akanksha Shukla <63357474+sakanksha19@users.noreply.github.com>
2021-10-07 19:56:12 +0530

    Update and rename matrix_multiplication.cpp to matrix-multiplication.cpp

--- algorithms/CPlusPlus/Arrays/matrix-multiplication.cpp
+++ algorithms/CPlusPlus/Arrays/matrix-multiplication.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main()
 {
     int n,m,n1,m1;
-    cout<<"Enter the dimensions of first matrix\n";
+   // cout<<"Enter the dimensions of first matrix\n";
     cin>>n>>m;
     int a[n][m];
-    cout<<"Enter the elements of first matrix\n";
+   // cout<<"Enter the elements of first matrix\n";
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<m;j++)
@@ -17,10 +17,10 @@ int main()
             cin>>a[i][j];
         }
     }
-    cout<<"Enter the dimensions of Second matrix\n";
+    //cout<<"Enter the dimensions of Second matrix\n";
     cin>>n1>>m1;
     int b[n1][m1];
-    cout<<"Enter the elements of second matrix\n";
+    //cout<<"Enter the elements of second matrix\n";
     for(int i=0;i<n1;i++)
     {
         for(int j=0;j<m1;j++)
@@ -30,7 +30,7 @@ int main()
     }
     if(m!=n1)
     {
-        cout<<"Multiplication of the matrices is not possible";
+        //cout<<"Multiplication of the matrices is not possible";
     }
     else
     {
@@ -63,3 +63,14 @@ int main()
     }    
     return 0;
 }
+//INPUT
+//2 3
+//1 2 1
+//3 4 1
+//3 2
+//1 2 
+//1 1
+//3 7
+//OUTPUT
+//6 11 
+//10 17 
