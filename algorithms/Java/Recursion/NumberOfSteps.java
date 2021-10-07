f8ed0eaf7d243ae95ff5602b848ab4b7db2efb77
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-10-07 19:14:38 +0530

    Add Recursion Problem

---
+++ algorithms/Java/Recursion/NumberOfSteps.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(numberOfStepsToZero(num));
+    }
+
+    private static int numberOfStepsToZero(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        if (num % 2 == 0) {
+            return 1 + numberOfStepsToZero(num / 2);
+        }
+        return 1 + numberOfStepsToZero(num - 1);
+    }
+}
