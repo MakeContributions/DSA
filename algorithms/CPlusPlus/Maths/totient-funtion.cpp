7f0e116122279632aff7cd94119e56072cc5de10
Kpriyansh <71462800+Kpriyansh@users.noreply.github.com>
2021-10-08 22:36:41 +0530

    add totient function

--- algorithms/CPlusPlus/Maths/totient-funtion.cpp
+++ algorithms/CPlusPlus/Maths/totient-funtion.cpp
@@ -1,3 +1,22 @@
+/*
+Program Description:
+Euler's totient function (also called the Phi function) counts the number of positive integers less than n that are coprime to n. That is, totient(n) is the number of m∈N such that 1≤m<n and gcd(m,n)=1.
+You are given an integer N and are supposed to find the value of Euler toient function for N.
+Constraints:
+1 <= N <= 10^9
+
+Approach:
+totient(N) = N*(1 - 1/p1)*(1 - 1/p2)...*(1 - 1/pn).
+where, p1,p2...pn are prime factors of n.
+So, we can write the above function as,
+totient(N) = N*[(p1-1)(p2-1)...(pn-1)/(p1*p2*p3...*pn)]
+1)Calculate all the primefactors of N.
+2)Calculate the numerator and denominator i.e N*(p1-1)(p2-1)...(pn-1) and (p1*p2*p3...*pn)
+3)Divide numerator by denominator.
+
+Time Complexity: O(NloglogN)
+*/
+
 #include<iostream>
 #include<vector>
 using namespace std;
