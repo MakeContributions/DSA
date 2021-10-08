da7bf24ede5677df17e758662196529829f48ca2
Elena Mokeeva <ryzheboka@gmail.com>
2021-10-08 20:11:20 +0200

    add two examples to main; add worst case time complexity and an explanation; fix the condition for exiting iteration through common substring, so that the loop stops at the end of the shorter string

--- algorithms/C/strings/longest-common-subsequence.c
+++ algorithms/C/strings/longest-common-subsequence.c
@@ -1,4 +1,15 @@
-/*  Program to find the longest common subsequence of two strings  */
+/*  Program to find the longest common subsequence of two strings.
+Example: longest common subsequence of "abcde" and "abzyxcde" is "cde".
+
+Worst case time complexety: O(n*m^2), where n is the length of the first string,
+                            and m is the length of the second string.
+                            Example strings for worst case time: "aaaaa" and "aaaaa"
+                            Explanation: for each character in the first string, you 
+                            iterate through the second string. For each character of
+                            the second string, you iterate through the common sequence
+                            that starts at that character. In the worst case,
+                            for each character in the second string, you iterate through
+                            the remaining second string. That's why the algorithm is O(n*m^2) */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -12,13 +23,38 @@ int main()
         "Hello World! Ciao Mondo! Hei Maailma!" 
         and "Bonjour Le Monde! Hei Maailma! Ciao Mondo!" */
     
-    char* first_seqence = "Hello World! Ciao Mondo! Hei Maailma!";
-    char* second_sequence = "Bonjour Le Monde! Hei Maailma! Ciao Mondo!";
-    char* longest_subsequence = longest_common_subsequence(first_seqence, second_sequence);
-    printf("%s\n", longest_subsequence);
-    free(longest_subsequence);
+    char* first_seqence1 = "Hello World! Ciao Mondo! Hei Maailma!";
+    char* second_sequence1 = "Bonjour Le Monde! Hei Maailma! Ciao Mondo!";
+    char* longest_subsequence1 = longest_common_subsequence(first_seqence1, second_sequence1);
+    printf("%s\n", longest_subsequence1);
+    free(longest_subsequence1);
 
     /* Output: "! Hei Maailma!"*/
+
+    /* Example: Finding longest common subsequence of following Strings
+        "abcde"
+        and "abzyxcde" */
+
+    char* first_seqence2 = "abcde";
+    char* second_sequence2 = "abzyxcde";
+    char* longest_subsequence2 = longest_common_subsequence(first_seqence2, second_sequence2);
+    printf("%s\n", longest_subsequence2);
+    free(longest_subsequence2);
+
+    /* Output: "cde"*/
+
+    /* Example: Finding longest common subsequence of following Strings
+        "aaaaaa"
+        and "aaa" */
+
+    char* first_seqence3 = "aaaaaa";
+    char* second_sequence3 = "aaa";
+    char* longest_subsequence3 = longest_common_subsequence(first_seqence3, second_sequence3);
+    printf("%s\n", longest_subsequence3);
+    free(longest_subsequence3);
+
+    /* Output: "aaa"*/
+
 }
 
 /* Function which compares two character sequences and returns a pointer
@@ -37,9 +73,12 @@ char* longest_common_subsequence(char *string1, char *string2)
         //iterating through string2 to check for common substring
         while(*string2!='\0')
         {
-            // iterating through the common substring, to see how long it is
+            /* iterating through the common substring, to see how long it is. 
+	    Checking each character, if the subsequence doesn't match any longer,
+            of if one of the strings has ended */
             while (*(string2+index_in_current_subsequence) == 
-*(string1+index_in_current_subsequence))
+*(string1+index_in_current_subsequence) && *(string2+index_in_current_subsequence) != '\0' &&
+*(string2+index_in_current_subsequence) != '\0')
             {
                 index_in_current_subsequence++;
             }
