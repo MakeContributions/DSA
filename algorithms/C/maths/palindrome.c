46610edb71d89ef3bac65a7c24f35a20e821c020
Priyansha Nayak <87187181+PriyanshaNayak@users.noreply.github.com>
2021-10-12 11:39:48 +0530

    added a program for contribution

--- algorithms/C/maths/palindrome.c
+++ algorithms/C/maths/palindrome.c
@@ -1,34 +1,21 @@
-// This program is used to calculate palindrome number in C
-// a palindrome number is a number which is same on reversing that number
-
-#include <stdio.h>		//header
-int main ()				//main function
+#include<stdio.h>
+int main()
 {
-  int num, rev = 0, r, s;	//define variables
-  printf (" enter the number: ");
-  scanf ("%d", &num);		// taking input
-  s = num;			//assigning num to s
-  while (s > 0)
-    {				// loop starts
-      r = s % 10;
-      rev = rev * 10 + r;	// calculating reverse
-      s = s / 10;
-    }
-  if (num == rev)
-    {				// checking if condition
-      printf ("Palindrome");	//printing if number is palindrome
-    }
-  else
-    {
-      printf ("Non-palindrome");	//printing if number is not palindrome
-    }
-  return 0;			//returning main function
+int n,reversed=0,remainder,original;
+printf("Enter an integer: ");
+scanf("%d",&n);
+original = n;
+//Reversed integer is stored in reverse variable
+while(n!=0)
+{
+  remainder = n%10;
+  reversed = reversed * 10 + remainder;
+  n/=10;
 }
-
-//      sample input    ||      sample output
-//          121         ||      Palindrome
-//          438         ||      Non-palindrome
-//          787         ||      Palindrome
-//          975         ||      Non-Palindrome
-
-// This program is having time complexity of O(N).
+//palindrome if original and reversed are equal
+if(original == reversed)
+printf("%d is a Palindrome");
+else
+printf("%d is not a palindrome");
+return 0; 
+}
