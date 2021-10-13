25a4198bcd217f40cf9e2032e75eff0919dda324
Samruddhi Ghodake <72791227+samughodake@users.noreply.github.com>
2021-10-13 12:53:27 +0530

    Update string-length.cpp
    added time complexity in comment of the program

--- algorithms/CPlusPlus/Recursion/string-length.cpp
+++ algorithms/CPlusPlus/Recursion/string-length.cpp
@@ -3,8 +3,10 @@ Description: find length of string using recursion.
 
 Approach: To run the recursive call till the end of the string.
 Maintaining a iterator variable which will increment at every recursive call
-If we reach at the end of the string, return the value of iterator vraiable
-which will be the length of the string
+If we reach at the end of the string, return the value of the iterator variable
+which will be the length of the string.
+
+Time Complexity: O(n) where n is the length of the string
 */
 #include <iostream>
 using namespace std;
