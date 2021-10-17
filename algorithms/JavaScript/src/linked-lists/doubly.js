e4a499ee18d42609a014b328014a561fb23687d2
Atul Nirbhavane <anirbhavane106@gmail.com>
2021-10-17 17:28:55 +0530

    fix deleting tail

--- algorithms/JavaScript/src/linked-lists/doubly.js
+++ algorithms/JavaScript/src/linked-lists/doubly.js
@@ -1,6 +1,5 @@
-
 function DoubleLinkedList() {
-  const Node = function(element) {
+  const Node = function (element) {
     this.element = element;
     this.next = null;
     this.prev = null;
@@ -11,19 +10,18 @@ function DoubleLinkedList() {
   let tail = head;
 
   // Add new element
-  this.append = function(element) {
+  this.append = function (element) {
     const node = new Node(element);
 
     node.prev = tail;
     tail.next = node;
     tail = node;
 
-
     length++;
   };
 
   // Add element
-  this.insert = function(position, element) {
+  this.insert = function (position, element) {
     // Check of out-of-bound values
     if (position >= 0 && position <= length) {
       const node = new Node(element);
@@ -49,10 +47,11 @@ function DoubleLinkedList() {
   };
 
   // Remove element at any position
-  this.removeAt = function(position) {
+  this.removeAt = function (position) {
     // look for out-of-bounds value
 
     if (position > -1 && position < length) {
+      let removedNode;
       let current = head;
       let index = 0;
 
@@ -60,21 +59,29 @@ function DoubleLinkedList() {
         current = current.next;
         index++;
       }
+      if (current.next) {
+        removedNode = current.next;
+      }
       if (current.next.next !== null) {
         current.next.next.prev = current;
       }
 
       current.next = current.next.next;
 
+      // if current.next is null, that means it is the last elem, so let's mark it as tail.
+      if (!current.next) {
+        tail = current;
+      }
+
       length--;
-      return current.element;
+      return removedNode.element;
     } else {
       return null;
     }
   };
 
   // Get the indexOf item
-  this.indexOf = function(elm) {
+  this.indexOf = function (elm) {
     let current = head;
     let index = -1;
 
@@ -97,7 +104,7 @@ function DoubleLinkedList() {
     return this.removeAt(this.indexOf(elm));
   };
 
-  this.toString = function() {
+  this.toString = function () {
     let current = head.next;
     let string = '';
 
@@ -110,7 +117,7 @@ function DoubleLinkedList() {
   };
 
   // Convert list to array
-  this.toArray = function() {
+  this.toArray = function () {
     const arr = [];
     let current = head.next;
 
@@ -123,26 +130,25 @@ function DoubleLinkedList() {
   };
 
   // Check if list is empty
-  this.isEmpty = function() {
+  this.isEmpty = function () {
     return length === 0;
   };
 
   // Get the size of the list
-  this.size = function() {
+  this.size = function () {
     return length;
   };
 
   // Get the head
-  this.getHead = function() {
+  this.getHead = function () {
     return head.next;
   };
 
   // Get the tail
-  this.getTail = function() {
+  this.getTail = function () {
     return tail;
   };
 }
-
 
 const newDoubleLinkedList = new DoubleLinkedList();
 newDoubleLinkedList.append(1);
@@ -156,4 +162,4 @@ console.log(newDoubleLinkedList.toArray());
 console.log(newDoubleLinkedList.toString());
 
 newDoubleLinkedList.delete(20);
-console.log('After delete 20: '+newDoubleLinkedList.toString());
+console.log('After delete 20: ' + newDoubleLinkedList.toString());
