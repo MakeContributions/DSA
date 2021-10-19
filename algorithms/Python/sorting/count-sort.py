d5d21a91e47de7ff9a0e2843c376258050260791
Akanksha Shukla <63357474+sakanksha19@users.noreply.github.com>
2021-10-19 22:26:07 +0530

    Update count-sort.py

--- algorithms/Python/sorting/count-sort.py
+++ algorithms/Python/sorting/count-sort.py
@@ -1,29 +1,42 @@
-#code in python for countsort algorithm
-n=int(input("Enter the size of array"))
+#Code in python for count sort 
+def count_sort(a):
+    max_element = int(max(a))
+    min_element = int(min(a))
+    range_of_elements = max_element - min_element + 1
+    # Create a count array to store count of individual
+    # elements and initialize count array as 0
+    c= [0 for _ in range(range_of_elements)]
+    o= [0 for _ in range(len(a))]
+    for i in range(0, len(a)):
+        c[a[i]-min_element] += 1
+    for i in range(1, len(c)):
+        c[i] += c[i-1]
+    for i in range(len(a)-1, -1, -1):
+        o[c[a[i] - min_element] - 1] = a[i]
+        c[a[i] - min_element] -= 1
+    for i in range(0, len(a)):
+        a[i] = o[i]
+ 
+    return a
+n=int(input("Enter the size of list"))
 a=[]
-c=[0 for i in range(0,1000001)]
-b=[0 for i in range(0,n)]
 for i in range(0,n):
     x=int(input())
     a.append(x)
-    c[a[i]]=c[a[i]]+1
-for i in range(1,1000001):
-    c[i]=c[i-1]+c[i]
-for i in range(0,n):
-    c[a[i]]=c[a[i]]-1
-    b[c[a[i]]]=a[i]
-for i in b:
-    print(i,end=" ")
+ans = count_sort(a)
+print("Sorted array is ")
+print(ans)
 
-    
 #INPUT/OUTPUT
-#Enter the size of array 5
+#Enter the size of list 5
 #6
-#4
 #7
-#2
-#1
-#1 2 4 6 7 
+#-1
+#-2
+#0
+#Sorted array is 
+#[-2, -1, 0, 6, 7]
 
 #TIME COMPLEXITY
 #O(n+k) where n is the number of elements in input array and k is the range of input 
+
