a86b9f9085d0bf61e2a817958c5d9d4053783c16
adeity21 <71463494+adeity21@users.noreply.github.com>
2021-10-28 19:27:58 +0530

    added description and time complexity

--- algorithms/CPlusPlus/Greedy/huffman_algo.cpp
+++ algorithms/CPlusPlus/Greedy/huffman_algo.cpp
@@ -1,3 +1,9 @@
+// Huffman coding is a lossless data compression algorithm. 
+// The idea is to assign variable-length codes to input characters, lengths of the assigned codes are based on the frequencies of corresponding characters.
+// The most frequent character gets the smallest code and the least frequent character gets the largest code.
+
+// Time complexity: O(nlogn)
+
 #include <bits/stdc++.h>
 using namespace std;
 
