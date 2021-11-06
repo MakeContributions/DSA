263d8fccb88a8abc3f3f070b131e5384f3c8ede9
samughodake <samughodake1808@gmail.com>
2021-11-06 10:09:33 +0530

    added programs

---
+++ algorithms/CPlusPlus/Recursion/linear-search.cpp
@@ -0,0 +1,47 @@
+/*
+Description: linear search using recursion
+
+Time Complexity: O(n) where n is the number of elements in the array
+*/
+
+#include <iostream>
+using namespace std;
+
+//function starts
+int linearSearch(int arr[], int i, int n, int target)
+{
+    //base case
+    if (i == n)
+    {
+        return -1;
+    }
+    //if the ith element in the array is equal to the target, return the index
+    if (arr[i] == target)
+    {
+        return i;
+    }
+    //recursive function call
+    //i refers to the index that we will need to check the element in the array
+    //at every recursive call we will increment the i by 1
+    return linearSearch(arr, i + 1, n, target);
+}
+
+//main starts
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 3;
+    cout << linearSearch(arr, 0, n, target);
+    return 0;
+}
+
+/*
+Input:
+arr = [1,2,3,4,5]
+target = 3
+
+Output:
+2
+
+*/
