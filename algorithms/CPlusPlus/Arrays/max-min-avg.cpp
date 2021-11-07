06ddaa8a9b2ec0c3705cf02b8d4e92c87ec36628
manekasingh05 <77896836+manekasingh05@users.noreply.github.com>
2021-11-08 00:35:19 +0530

    Create max-min-avg.cpp
    Maximum, Minimum and average in an array

---
+++ algorithms/CPlusPlus/Arrays/max-min-avg.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int max(int *arr, int k, int size)         //function to find maximum element in an array
+{
+    int i;
+for(i=1; i<size; i++)
+{
+    if(arr[i]>k)
+    {
+        k=arr[i];
+    }
+}
+return k;
+}
+
+int min(int *arr, int k, int size)     //function to find minimum element in an array
+{
+    int i;
+for(i=1; i<size; i++)
+{
+    if(arr[i]<k)
+    {
+        k=arr[i];
+    }
+}
+return k;
+}
+
+int avg(int *arr, int k, int size)            //function to find average of an array
+{
+    int sum=0,avg;
+    for(int i=0; i<size; i++)
+    {
+     sum=sum+arr[i];                        //finding sum of every element in an array
+    }
+     avg=sum/size;
+     return avg;
+}
+
+int main()
+{
+    int size;
+    int k=0;
+    cout<<"Enter the size of an array\n";
+    cin>>size;
+    int *arr=new int[size];                   //dynamic memory allocation 
+    cout<<"Enter elements in an array\n";
+    for (int i = 0; i < size; i++)
+    {
+        cin>>arr[i];
+    }
+    k=arr[0];
+    cout<<"Maximum element is:\n"<<max(arr,k,size)<<"\n";
+    cout<<"Minimum element is:\n"<<min(arr,k,size)<<"\n";
+    cout<<"Average will be:\n"<<avg(arr,k,size)<<"\n";
+    return 0;
+}
