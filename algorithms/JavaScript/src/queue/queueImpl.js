d2d39d54619006d02b12c10cbe3954175d144ff4
Atul Nirbhavane <anirbhavane106@gmail.com>
2021-11-22 05:19:01 +0530

    Indentation-fix

--- algorithms/JavaScript/src/queue/queueImpl.js
+++ algorithms/JavaScript/src/queue/queueImpl.js
@@ -1,51 +1,51 @@
 class Node {
-    constructor(value) {
-      this.value = value;
-      this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-  
-  class Queue {
-    constructor() {
-      this.first = null;
-      this.last = null;
-      this.size = 0;
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
     }
-  
-    enqueue(value) {
-      let newNode = new Node(value);
-      if (!this.first) {
-        this.first = newNode;
-        this.last = newNode;
-      } else {
-        this.last.next = newNode;
-        this.last = newNode;
+    this.size++;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+    const temp = this.first;
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+
+  printQueue() {
+      let current = this.first;
+      while (current) {
+          console.log(current.value);
+          current = current.next;
       }
-      this.size++;
-    }
-  
-    dequeue() {
-      if (!this.first) return null;
-      let temp = this.first;
-      this.first = this.first.next;
-      this.size--;
-      return temp.value;
-    }
-
-    printQueue(){
-        let current = this.first;
-        while(current){
-            console.log(current.value);
-            current = current.next;
-        }
-    }
   }
+}
 
-  const queue = new Queue();
-  queue.enqueue(1);
-  queue.enqueue(2);
-  queue.enqueue(3);
-  queue.enqueue(4);
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
 
   queue.printQueue();
 
--- algorithms/JavaScript/src/stack/stackImpl.js
+++ algorithms/JavaScript/src/stack/stackImpl.js
@@ -1,30 +1,29 @@
 class Stack {
-    constructor() {
-      this.items = [];
-    }
-  
-    push(element) {
-      this.items.push(element);
-    }
-  
-    pop() {
-      return this.items.pop();
-    }
-
-    printStack(){
-        console.log(this.items);
-    }
+  constructor() {
+    this.items = [];
   }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  printStack() {
+      console.log(this.items);
+  }
+}
   
-  const stack = new Stack();
-  stack.push(1);
-  stack.push(2);
-  stack.push(3);
-  stack.push(4);
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
 
-  stack.printStack();
+stack.printStack();
 
-  console.log(stack.pop());
+console.log(stack.pop());
 
-  stack.printStack();
-  
+stack.printStack();
