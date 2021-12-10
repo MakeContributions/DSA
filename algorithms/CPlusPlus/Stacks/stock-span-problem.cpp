db8137ddeb3a3f34ecebcd3ca9e2603bb6d58ab8
DEBJIT <karmadebjit@gmail.com>
2021-12-10 13:57:20 +0530

    Update stock-span-problem.cpp

--- algorithms/CPlusPlus/Stacks/stock-span-problem.cpp
+++ algorithms/CPlusPlus/Stacks/stock-span-problem.cpp
@@ -1,4 +1,3 @@
-
 /*The stock span problem is a financial problem where we have a series of n daily price quotes for a stock and we need to calculate span of stock’s price for all n days.*/
 /*The span Si of the stock’s price on a given day i is defined as the maximum number of consecutive days just before the given day, for which the price of the stock on the current day is less than its price on the given day.*/
 // C++ linear time solution for stock span problem
@@ -8,7 +7,7 @@ using namespace std;
  
 // A stack based efficient method to calculate
 // stock span values
-void calculateSpan(const vector<int> price, const int n, int S[])
+void calculateSpan(const vector<int> price, const int n,vector<int>& S)
 {
     // Create a stack and push index of first
     // element to it
@@ -16,7 +15,7 @@ void calculateSpan(const vector<int> price, const int n, int S[])
     st.push(0);
  
     // Span value of first element is always 1
-    S[0] = 1;
+    S.push_back(1);
  
     // Calculate span values for rest of the elements
     for (int i = 1; i < n; i++) {
@@ -31,7 +30,8 @@ void calculateSpan(const vector<int> price, const int n, int S[])
         // i.e., price[0], price[1], ..price[i-1].  Else
         // price[i] is greater than elements after
         // top of stack
-        S[i] = (st.empty()) ? (i + 1) : (i - st.top());
+        int x = (st.empty()) ? (i + 1) : (i - st.top());
+        S.push_back(x);
  
         // Push this element to stack
         st.push(i);
@@ -39,7 +39,7 @@ void calculateSpan(const vector<int> price, const int n, int S[])
 }
  
 // A utility function to print elements of array
-void printArray(const int arr[],const int n)
+void printArray(const vector<int>& arr,const int n)
 {
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
@@ -52,7 +52,7 @@ int main()
     int n;
     //Enter the size of price
     cin>>n;
-    int S[n];
+    vector<int> S;
     for(int i=0;i<n;i++)
      {
          int x;
@@ -62,7 +62,7 @@ int main()
  
     // Fill the span values in array S[]
     calculateSpan(price, n, S);
- 
+    
     // print the calculated span values
     printArray(S, n);
  
