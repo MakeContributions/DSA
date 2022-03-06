edbed12492d6137b15ad75ce12a4502491303e0c
T.K.Santhosh <81608070+santhoshtk01@users.noreply.github.com>
2022-03-06 22:23:03 +0530

    Changed typos

--- algorithms/Python/arrays/remove_duplicates_list.py
+++ algorithms/Python/arrays/remove_duplicates_list.py
@@ -9,7 +9,7 @@ sample_case = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 
 def make_distinct(values: list) -> list:
     """
-    Remove duplicate element in an array inplace without creating new array.
+    Remove duplicate elements in an array inplace without creating new array.
 
     Here, we are iterating the list backwards instead of forward because if we
     remove elements in an array it will cause some issues.
@@ -20,7 +20,7 @@ def make_distinct(values: list) -> list:
     length = len(values)
     for index in range(len(values)):
         # *index_position = 0 - 10*
-        # *index_position = 10*
+        # *index_position = -10*
         index_position = index - length
         if values[index_position] in values[0:index_position]:
             values.remove(values[index_position])
