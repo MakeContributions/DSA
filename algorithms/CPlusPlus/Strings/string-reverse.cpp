14b306d13773a31dc856f8d602c21690f4d23ff2
Shivam Patel <87613778+ShivamPatel-24@users.noreply.github.com>
2022-04-01 09:35:02 -0400

    Updated and added new implementation
    Added a new implementation using a while loop. Added a few helpful comments.

--- algorithms/CPlusPlus/Strings/string-reverse.cpp
+++ algorithms/CPlusPlus/Strings/string-reverse.cpp
@@ -2,11 +2,28 @@
 #include<string>
 #include<algorithm>
 using namespace std;
+
+string revStr(string s){
+    int i = 0, j = s.length()-1;
+    
+    while (i < j){
+        swap(s[i], s[j]);    
+        i++; j--;
+    }
+    return s;
+}
+
 int main() {
     string s;
+    cout << "Enter the string name: ";
     cin>>s;
-reverse(s.begin(),s.end());
-cout<<s;
-return 0;
+    
+    // manually reversing it. This is not in-place method, to update the string you would have to do s = revStr(s)
+    cout << revStr(s) << endl;
+    
+    // using a method from algorithm library. This is an in-place method.
+    reverse(s.begin(),s.end());
+    cout<< s << endl;
+    return 0;
 
 }
