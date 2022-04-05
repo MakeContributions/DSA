9febff6cb977fe51a9fff57c45ff065db4128a9d
Mohammadreza Heidari <m.heidari988@gmail.com>
2022-04-05 19:33:17 +0300

    Update algorithms/CSharp/test/Search/minima-maxima.cs
    Co-authored-by: Ming Tsai <37890026+ming-tsai@users.noreply.github.com>

--- algorithms/CSharp/test/Search/minima-maxima.cs
+++ algorithms/CSharp/test/Search/minima-maxima.cs
@@ -16,7 +16,7 @@ namespace Algorithms.Tests.Search
         }
 
         [TestCase(new int[] { 3, 1, 2, 5, 6, 7, 4, 6, 9, 10 }, new int[] { 3, 7, 10 })]
-        public void MinimaMaxima_GetListOfMaximas(int[] input, int[] result)
+        public void PassIntegerList_ShouldGetExpectedMaximas(int[] input, int[] result)
         {
             var expected1 = MinimaMaxima.GetLocalMaxima(input.ToList());
             Assert.AreEqual(expected1, result);
