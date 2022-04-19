5d272b29f0d7d7199e9ea5889cafbe0381bee99b
Matt <87787075+maty714@users.noreply.github.com>
2022-04-18 22:02:33 -0400

    Corrected character-Limit

--- algorithms/CSharp/src/Strings/character-limit.cs
+++ algorithms/CSharp/src/Strings/character-limit.cs
@@ -1,18 +1,34 @@
 using System;
 using System.Linq;
-using System.Text.RegularExpressions;
+
+
+//Time Complexity: O(N)
+
 
 namespace Algorithms.Strings{
 
     public class TextHelper{
+        
         public static void Main()
         {
             Console.WriteLine("Please enter a string");
             string input = Console.ReadLine();
-            Console.WriteLine("Please enter the number of characters you want to show");
-            int num = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(Character_Limit(input, num));
+           while(input == ""){
+               Console.WriteLine("Please enter a string");
+               input = Console.ReadLine();
+           } 
+            
+            Console.WriteLine("Please enter the number of characters you want to show");
+            string input2 = Console.ReadLine();
+            
+            while (!input2.All(char.IsDigit) || input2.All(char.IsWhiteSpace)){
+                Console.WriteLine("The number you have entered is invalid. Please try again:");
+                input2 = Console.ReadLine();
+            }
+
+           int num = Convert.ToInt32(input2);
+           Console.WriteLine(Character_Limit(input, num));
         }
 
         //Limits the the amount of characters shown in a string.
@@ -21,7 +37,6 @@ namespace Algorithms.Strings{
 
             //converts a string to a character array
             char[] ch = input.ToCharArray();
-
 
             //loops through the array from the last element to the first, and replaces each element with a "."
             for (int i = ch.Count(); i > num; i--)
@@ -32,13 +47,6 @@ namespace Algorithms.Strings{
             //converts character array back to string to be returned by the method
             string output = new string(ch);
             return output;
-
-
-
         }
-
-
-
-
     }
 }
---
+++ algorithms/CSharp/test/Strings/character-limit.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace Algorithms.CSharp.Test.Strings
+{
+    [TestFixture]
+    internal class Character_Limit
+    {
+        [TestCase("Hello", "Hel..")]
+        [TestCase("World", "Wor..")]
+        [TestCase("How Ya", "How...")]
+        [TestCase("Doin", "Doi.")]
+
+
+        public void PassString_ShouldGetExpectedResult(string text, string expected)
+        {
+            string result = Algorithms.Strings.TextHelper.Character_Limit(text, 3);
+            Assert.IsTrue(result == expected, "error");
+        }
+    }
+}
