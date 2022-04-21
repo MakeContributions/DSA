8f20ac115767a89f74b5736a45d98bdc96a5a0ad
Ming Tsai <37890026+ming-tsai@users.noreply.github.com>
2022-04-21 09:28:50 -0400

    Update algorithms/CSharp/test/Strings/character-limit.cs

--- algorithms/CSharp/test/Strings/character-limit.cs
+++ algorithms/CSharp/test/Strings/character-limit.cs
@@ -11,7 +11,7 @@ namespace Algorithms.CSharp.Test.Strings
         [TestCase("Doin", "Doi.")]
         public void PassString_ShouldGetExpectedResult(string text, string expected)
         {
-            string result = Algorithms.Strings.TextHelper.Character_Limit(text, 3);
+            string result = Algorithms.Strings.TextHelper.CharacterLimit(text, 3);
             Assert.AreEqual(expected, result);
         }
     }
