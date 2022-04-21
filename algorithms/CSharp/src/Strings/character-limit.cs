e1412d541e74863024a6e5f026abc9c79ecec472
Matt <87787075+maty714@users.noreply.github.com>
2022-04-20 23:44:36 -0400

    Format code
    Co-authored-by: Ming Tsai <37890026+ming-tsai@users.noreply.github.com>

--- algorithms/CSharp/src/Strings/character-limit.cs
+++ algorithms/CSharp/src/Strings/character-limit.cs
@@ -1,40 +1,38 @@
 using System;
 using System.Linq;
 
-
 //Time Complexity: O(N)
-
-
-namespace Algorithms.Strings{
-
-    public class TextHelper{
-        
+namespace Algorithms.Strings
+{
+    public class TextHelper
+    {
         public static void Main()
         {
             Console.WriteLine("Please enter a string");
             string input = Console.ReadLine();
 
-           while(input == ""){
-               Console.WriteLine("Please enter a string");
-               input = Console.ReadLine();
-           } 
-            
+            while (input == "")
+            {
+                Console.WriteLine("Please enter a string");
+                input = Console.ReadLine();
+            }
+
             Console.WriteLine("Please enter the number of characters you want to show");
             string input2 = Console.ReadLine();
-            
-            while (!input2.All(char.IsDigit) || input2.All(char.IsWhiteSpace)){
+
+            while (!input2.All(char.IsDigit) || input2.All(char.IsWhiteSpace))
+            {
                 Console.WriteLine("The number you have entered is invalid. Please try again:");
                 input2 = Console.ReadLine();
             }
 
-           int num = Convert.ToInt32(input2);
-           Console.WriteLine(Character_Limit(input, num));
+            int num = Convert.ToInt32(input2);
+            Console.WriteLine(Character_Limit(input, num));
         }
 
         //Limits the the amount of characters shown in a string.
         public static string Character_Limit(string input, int num)
         {
-
             //converts a string to a character array
             char[] ch = input.ToCharArray();
 
