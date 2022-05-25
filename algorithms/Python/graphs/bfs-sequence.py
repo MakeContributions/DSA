f08f5fbf9e1df6b8f706cd1862185e8f2f99433b
Nibedita Chakraborty <96379756+NiviRocks@users.noreply.github.com>
2022-05-25 22:13:29 +0530

    added adjacency list using dictionary

--- algorithms/Python/graphs/bfs-sequence.py
+++ algorithms/Python/graphs/bfs-sequence.py
@@ -21,25 +21,17 @@ class Graph:
     # front and rear for accessing queue
     front=0
     rear=0
-    # stores Adjecency List as dictionary for easy usage
-    Adj_Dict={}
-    def graph(Adj_Mtx): #converts Adj_MTX into Adj_Dict
-        for i in Adj_Mtx:
-            if i[0] not in Graph.Adj_Dict:
-                Graph.Adj_Dict[i[0]]=[i[1]]
-            else:
-                Graph.Adj_Dict[i[0]].append(i[1])
-    def ShowGraph(): #displays graph
-        for i in Graph.Adj_Dict:
-            print(i,"->",Graph.Adj_Dict[i])
-    def Display_BFS(curr): # displays BFS sequence
+    def ShowGraph(Adj_Dict): #displays graph
+        for i in Adj_Dict:
+            print(i,"->",Adj_Dict[i])
+    def Display_BFS(curr,Adj_Dict): # displays BFS sequence
         print(curr,end=" ")
-        if curr in Graph.Adj_Dict :
+        if curr in Adj_Dict :
             if curr not in Graph.visited: 
                 Graph.visited.append(curr) 
                 Graph.queue.append(curr) 
                 Graph.rear+=1
-            for i in Graph.Adj_Dict[curr]: # iterate over all neighbours of curr
+            for i in Adj_Dict[curr]: # iterate over all neighbours of curr
                 if i not in Graph.visited: 
                     Graph.queue.append(i)
                     Graph.visited.append(i)
@@ -49,22 +41,21 @@ class Graph:
         if Graph.front==Graph.rear: # no new node to visit
             return
         else:
-            Graph.Display_BFS(Graph.queue[Graph.front]) # go to next node
+            Graph.Display_BFS(Graph.queue[Graph.front],Adj_Dict) # go to next node
         return
 
 #__main__
 #Dry Run
-g=[[1,2],[1,4],[2,4],[2,5],[4,7],[4,5],[5,3],[5,1],[5,6],[6,8],[6,3],[8,7]] # this is directed graph
+#g is an adjecency list in form of dictionary
+g={1:[2,4],2:[4,5],4:[7,5],5:[1,3,6],6:[3,8],8:[7]} # this is directed graph
 # for undirected graph each edge has to be given twice
-# Eg:- edge from 1-2 input as [[1,2],[2,1]]
-Graph.graph(g) #create graph
-print("Display Graph")
-Graph.ShowGraph()
+# Eg:- edge from 1-2 input as {1:[2]}
+Graph.ShowGraph(g)
 print("BFS Sequence")
-Graph.Display_BFS(1) #passing start node
+Graph.Display_BFS(1,g) #passing start node
 '''
-OUTPUT:-
 ---------------------------------
+OUTPUT:-
 Display Graph
 1 -> [2, 4]
 2 -> [4, 5]
@@ -76,10 +67,9 @@ BFS Sequence
 1 2 4 5 7 3 6 8 
 ----------------------------------
 TIME COMPLEXITY:
-----------------------------------
--For Adjecency List Only!
- O(V+E) where V and E are number
+-O(V+E) where V and E are number
  of vertices and egdes in graph 
  respectively.
+-For Adjecency List Only!
 ----------------------------------
 '''
