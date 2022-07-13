684d655491f941b66fdb1adc907502eb0e90ddbf
Prathamesh Sahasrabuddhe <prathamesh16020@gmail.com>
2022-07-13 21:06:46 +0530

    Added output example in Depth First Search algorithm

--- algorithms/Python/recursion/depth-first-search.py
+++ algorithms/Python/recursion/depth-first-search.py
@@ -1,5 +1,25 @@
 # A dfs alorithm in Python
 
+"""
+Refer the following example for output
+Graph: https://cdn.programiz.com/sites/tutorial2program/files/graph-dfs-step-0.png
+Output:
+Enter number of nodes: 5    
+Enter nodes connected to node 0: 
+1 2 3
+Enter nodes connected to node 1: 
+1 2
+Enter nodes connected to node 2: 
+0 1 4
+Enter nodes connected to node 3: 
+0
+Enter nodes connected to node 4: 
+2
+DFS is:
+0 1 2 4 3
+
+"""
+
 n = int(input("Enter number of nodes: "))
 vis = []
 for i in range(0,n):
