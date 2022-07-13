269ec7cea4cf57d7c3c704ff1c014422947ecf83
Prathamesh Sahasrabuddhe <prathamesh16020@gmail.com>
2022-07-14 04:24:20 +0530

    bug fixes

--- algorithms/Python/recursion/depth-first-search.py
+++ algorithms/Python/recursion/depth-first-search.py
@@ -1,46 +1,62 @@
 # A dfs alorithm in Python
+# This is an algorithm for traversing a graph in depth first search manner
+# It is commonly used in tree/graph traversals
+# Time Complexity: O(V+E)
+# Space Complexity: O(V)
+# V is number of vertices and E is number of edges
 
 """
 Refer the following example for output
-Graph: https://cdn.programiz.com/sites/tutorial2program/files/graph-dfs-step-0.png
-Output:
-Enter number of nodes: 5    
-Enter nodes connected to node 0: 
-1 2 3
-Enter nodes connected to node 1: 
-1 2
-Enter nodes connected to node 2: 
-0 1 4
-Enter nodes connected to node 3: 
-0
-Enter nodes connected to node 4: 
-2
-DFS is:
-0 1 2 4 3
+Graph 1: https://cdn.programiz.com/sites/tutorial2program/files/graph-dfs-step-0.png
+Graph 2: https://static.javatpoint.com/ds/images/depth-first-search-algorithm2.png
 
 """
 
-n = int(input("Enter number of nodes: "))
-vis = []
-for i in range(0,n):
-	vis.append(0)
-
-def dfs(edges, node):
+def dfs(edges, vis, node):
 	if(vis[node]!=1):
 		vis[node] = 1
 		print(node, end = " ")
 	
 	for i in edges[node]:
 		if(vis[i]!=1):
-			dfs(edges, i)
+			dfs(edges, vis, i)
 
 
-edges = []
+def main():
+	""" Main Function """
 
-for i in range(0,n):
-	print(f"Enter nodes connected to node {i}: ")
-	destinations = list(map(int, input().split()))
-	edges.append(destinations)
+	#First example
 
-print("DFS is:")
-dfs(edges, 0)
+	graph1 = []
+	graph1.append([1,2,3])
+	graph1.append([0,2])
+	graph1.append([0,1,4])
+	graph1.append([0])
+	graph1.append([2])	
+
+	vis = []
+	for i in range(0,10):
+		vis.append(0)
+
+	print("DFS for Graph 1 is:")
+	dfs(graph1, vis, 0)
+
+	#Making visited list elements 0 for second graph
+	for i in range(0,10):
+		vis[i] = 0
+
+	graph2 = []
+	graph2.append([1, 2, 3])
+	graph2.append([3])
+	graph2.append([4])
+	graph2.append([5, 6])
+	graph2.append([5, 7])
+	graph2.append([2])
+	graph2.append([])
+	graph2.append([])
+
+	print("\n\nDFS for Graph 2 is:")
+	dfs(graph2, vis, 0)
+
+if __name__=="__main__":
+	main()
