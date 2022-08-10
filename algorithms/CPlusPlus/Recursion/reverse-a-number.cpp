9aa98a305526d515add49a7487e0d0e2ba39dddc
Ashad <93534298+Ashad001@users.noreply.github.com>
2022-08-10 08:43:52 +0500

    Update reverse-a-number.cpp

--- algorithms/CPlusPlus/Recursion/reverse-a-number.cpp
+++ algorithms/CPlusPlus/Recursion/reverse-a-number.cpp
@@ -1,33 +1,34 @@
 /*
 Description: Program to reverse integer using recursion
 Time complexity: O(n) where n is the number of digits in the integer
-
 */
 
 #include <iostream>
 #include <math.h>
 using namespace std;
 
+int Helper(int n ,int base , int ans)
+{
+    if(n < 1)
+        return ans;
+    ans = ans * base + (n % 10);    // Update the ans for every digit
+    return Helper(n / 10, base , ans);    
+}
+
 int Rev(int n)
 {
-    // the following variable is static means that it is only applicable for the first call of a function.
-    static long base = pow(10 , (int)log10(n) + 1); // Count how many units are there in the number.
-    if(n < 10) 
-        return n;
-    base /= 10; // Divide the base by 10.
-    return (n % 10) * base + Rev(n / 10);    // take the remainder a number and take to base units
-    // Ex: 12345 will lead to 50000 in the first call.
-    // and then reduce the number by 10.
+    return Helper(n , 10, 0);
 }
 
 
 int main(int argc, char const *argv[])
 {   
-    int integer = 13579;
-    cout << Rev(integer);    
+    cout << Rev(13579);
+
     return 0;
 }
 /*
 Input: 13579
 Output: 97531
 */
+
