46961ee18f6b602a665eca37427e52a8f69cca69
WORLDSAVER <pareekhridyansh@gmail.com>
2022-08-13 21:30:57 +0530

    Added GCD using Euclid's algorithm in the Recursion Folder

--- algorithms/Python/recursion/gcd_using_recursion.py
+++ algorithms/Python/recursion/gcd_using_recursion.py
@@ -1,9 +1,23 @@
-def gcd(a, b):
+#EUCLIDS ALGORITHM 
 
+def gcd(a, b):
+        if a<0:
+                a=-a
+        if b<0:
+                b =-b
 	if b==0:
 		return a  
 	else:
 		return gcd(b, a%b)
 
-#EUCLIDS ALGORITHMS 
+
 #TIME COMPLEXITY - O(log(min(a, b)))
+#EXAMPLES
+#print(gcd(10, 2)) -> 2
+#print(gcd(30, 15)) -> 3
+#print(gcd(-30, 15)) -> 3
+#WORKING
+#We are using the Euclidean Algorithm to calculate the GCD of a number
+#it states that if a and b are two positive integers then GCD or greatest common
+#divisors of these two numbers will be equal to the GCD of b and a%b or
+#gcd(a, b) = gcd(b, a%b) till b reaches 0
