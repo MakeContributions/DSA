7f1f8e99e75346966738cc7da4c864844cc359b1
WORLDSAVER <pareekhridyansh@gmail.com>
2022-08-15 08:36:46 +0530

    Fixed Indentation errors

--- algorithms/Python/recursion/gcd_using_recursion.py
+++ algorithms/Python/recursion/gcd_using_recursion.py
@@ -1,10 +1,10 @@
 #EUCLIDS ALGORITHM 
 
 def gcd(a, b):
-        if a<0:
-                a=-a
-        if b<0:
-                b =-b
+	if a<0:
+		a = -a  
+	if b<0:
+		b = -b
 	if b==0:
 		return a  
 	else:
