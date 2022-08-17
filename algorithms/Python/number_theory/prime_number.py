7e89f1a2d00bef927d1505c624d6c4c0ab4d726a
Hridyansh Pareek <pareekhridyansh@gmail.com>
2022-08-17 21:32:56 +0530

    Update prime_number.py

--- algorithms/Python/number_theory/prime_number.py
+++ algorithms/Python/number_theory/prime_number.py
@@ -2,7 +2,9 @@
 
 
 def isPrime(N):
-
+        if N<=1:
+                return False
+        
         for i in range(2, int(N**0.5) + 1):
                 if N%i==0:
                         return False
