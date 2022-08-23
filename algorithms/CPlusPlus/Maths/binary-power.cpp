f66851cf5516d9c39667df4d70af08cc841671fc
angshudas <offangshudas123@gmail.com>
2022-08-23 16:54:22 +0530

    binary-power in cpp implemented

---
+++ algorithms/CPlusPlus/Maths/binary-power.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// using recursion to find exponentiation
+long power(long base ,long expo){
+  // if exponent is equal to 0 then return 0
+  if (expo==0) 
+    return 1;
+  
+  // storing the value of a^(x/2) to minimize recursive call
+  long ans= power(base , expo/2);
+
+  if (expo%2) return base*ans*ans;
+  else return ans*ans;
+}
+
+
+int main()
+{
+  int a,n; cin>>a>>n;
+  cout << power(a,n);
+  return 0;
+}
