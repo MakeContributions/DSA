8277300b2aa8904fe113b9bd98bd4412cba7396a
angshudas <offangshudas123@gmail.com>
2022-08-24 15:41:36 +0530

    iterative solution added

--- algorithms/CPlusPlus/Maths/binary-power.cpp
+++ algorithms/CPlusPlus/Maths/binary-power.cpp
@@ -1,8 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// using recursion to find exponentiation
-long power(long base ,long expo){
+/*
+  RECURSIVE SOLUTION :-
+    Space Complexity - O( log(N) )
+    Time Complexity - O( log(N) )
+*/
+long power_recursion(long base ,long expo){
   // if exponent is equal to 0 then return 1
   // as a^0 = 1 for all numbers
   if (expo==0) 
@@ -16,9 +20,34 @@ long power(long base ,long expo){
 }
 
 
+/*
+    ITERATIVE SOLUTION :-
+      Space Complexity -> O(1)
+      Time Complexity -> O( log(N) )
+*/
+long power_iterative( long base, long expo ){
+  long res = 1; // inititalise result with 1
+  while( expo!=0 ){ // run loop while exponent is not 0
+
+    if( expo&1 ) // if least significant bit is 1 then update result 
+      res = res * expo;
+    
+    base = base * base;
+    expo>>=1;
+    // update base and right shift exponent by 1
+
+  }
+
+
+  // after loop is exited return result
+  return res;
+}
+
+
 int main()
 {
   int a,n; cin>>a>>n;
-  cout << power(a,n);
+  cout << power_recursion(a,n)<<endl;
+  cout<< power_iterative(a,n)<<endl;
   return 0;
 }
