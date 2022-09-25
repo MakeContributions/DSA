d81fa8c4dcc6a13d1c80ac5cfbecda1acf37e12e
Vishnu Rai <vishnunarayan1670@gmail.com>
2022-09-25 22:08:54 +0530

    Updated Ternery Search
    Added Time compleities and iterative method of the ternery search.

--- algorithms/Java/searching/ternery-search.java
+++ algorithms/Java/searching/ternery-search.java
@@ -1,9 +1,14 @@
 // Java program to illustrate
 // the iterative approach to ternary search
+// 
   
 class main {
   
     // Function to perform Ternary Search
+    // recursive Function
+    // Time Complexity: O(log3n)
+    // Auxiliary Space: O(log3n)
+  
     static int ternarySearch(int l, int r, int key, int ar[])
   
     {
@@ -47,6 +52,55 @@ class main {
         // Key not found
         return -1;
     }
+  
+     // Function to perform Iterative Ternary Search
+     // Time Complexity: O(log3n), where n is the size of the array.
+     // Auxiliary Space: O(1)
+  
+    static int ternarySearch(int l, int r, int key, int ar[])
+  
+    {
+        while (r >= l) {
+  
+            // Find the mid1  mid2
+            int mid1 = l + (r - l) / 3;
+            int mid2 = r - (r - l) / 3;
+  
+            // Check if key is present at any mid
+            if (ar[mid1] == key) {
+                return mid1;
+            }
+            if (ar[mid2] == key) {
+                return mid2;
+            }
+  
+            // Since key is not present at mid,
+            // check in which region it is present
+            // then repeat the Search operation
+            // in that region
+  
+            if (key < ar[mid1]) {
+  
+                // The key lies in between l and mid1
+                r = mid1 - 1;
+            }
+            else if (key > ar[mid2]) {
+  
+                // The key lies in between mid2 and r
+                l = mid2 + 1;
+            }
+            else {
+  
+                // The key lies in between mid1 and mid2
+                l = mid1 + 1;
+                r = mid2 - 1;
+            }
+        }
+  
+        // Key not found
+        return -1;
+    }
+  
   
     // Driver code
     public static void main(String args[])
