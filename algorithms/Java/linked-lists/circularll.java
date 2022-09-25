337511a4b2fd1d5b2e063684b43c9f3a1e3d3adc
Ritesh Yadav <90106115+riteshyadav27@users.noreply.github.com>
2022-09-25 17:45:50 +0530

    Singly Circular LinkedList
    Singly Circular LinkedList with functionality of Add, Add in front, Display, Reverse , Search element in LinkedList

---
+++ algorithms/Java/linked-lists/circularll.java
@@ -0,0 +1,95 @@
+public class circularll {
+        Node head;
+        Node tail;
+        class Node{
+            int data;
+            Node next;
+
+            Node(int data){
+                this.data=data;
+                this.next=null;
+            }
+        }
+
+        public void add(int data){
+            Node newNode = new Node(data);
+            if(head==null){
+                head=tail=newNode;
+            }
+            else{
+                tail.next=newNode;
+                tail=tail.next;
+            }
+            tail.next=head;
+        }
+
+        public void display(){
+            System.out.printf("\nThe Linkedlist is ");
+            Node temp=head;
+            do{
+                System.out.print(temp.data+" ");
+                temp=temp.next;
+            }while (temp!=head);
+        }
+
+    public void search(int target){
+        int flag=0;
+        Node temp=head;
+        do{
+           if(target==temp.data){
+               System.out.print("\nTarget is found");
+               flag=1;
+               break;
+           }
+            temp=temp.next;
+        }while (temp!=head);
+
+        if (flag==0)
+            System.out.print("\nTarget Not Found");
+    }
+        public void addfront(int data){
+            Node newNode = new Node(data);
+            newNode.next=head;
+            head=newNode;
+            tail.next=head;
+        }
+
+        public void reverse(){
+            Node previous=tail;
+            Node current=head,nextnode = head;
+
+
+            do{
+                nextnode=current.next;
+                current.next=previous;
+
+                previous=current;
+                current=nextnode;
+
+            }while(current!=head);
+
+            tail=head;
+            head=previous;
+            tail.next=head;
+
+        }
+
+        public static void main(String[] args) {
+            System.out.println("LinkedList");
+            circularll ll = new circularll();
+            ll.add(5);
+            ll.add(8);
+            ll.add(7);
+            ll.add(1);
+            ll.add(2);
+            ll.display();
+            ll.addfront(9);
+            ll.display();
+            ll.search(2);
+            ll.search(4);
+            ll.reverse();
+            ll.display();
+
+        }
+    }
+
