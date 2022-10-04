98888e88e98e8d0458f70b8bf3cdfa9d8f8cd8f3
Dhruv Bhat <52482148+spaceman-dev@users.noreply.github.com>
2022-10-05 00:00:44 +0530

    Create middle-element-of-linked-list.cpp

---
+++ algorithms/CPlusPlus/Linked-Lists/middle-element-of-linked-list.cpp
@@ -0,0 +1,87 @@
+/*
+Given a singly linked list of N nodes. The task is to find the middle of the linked list. For example, if given linked list is 1->2->3->4->5 then the output should be 3.
+If there are even nodes, then there would be two middle nodes, we need to print the second middle element. For example, if given linked list is 1->2->3->4->5->6 then the output should be 4.
+Example 1:
+Input: LinkedList: 1->2->3->4->5
+Output: 3 
+Explanation: Middle of linked list is 3.
+*/
+// { Driver Code Starts
+/* Function to get the middle of the linked list*/
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+} 
+/* Function to get the middle of the linked list*/
+int getMiddle(Node *head);
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		cout<<getMiddle(head)<<endl;
+	}
+	return 0; 
+} 
+
+// } Driver Code Ends
+
+
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+
+/* Should return data of middle node. If linked list is empty, then  -1*/
+int getMiddle(Node *head)
+{
+    // Your code here
+    if(!head)
+        return -1;
+    Node *slow = head, *fast = head;
+    while(fast && fast->next)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow->data;
+}
