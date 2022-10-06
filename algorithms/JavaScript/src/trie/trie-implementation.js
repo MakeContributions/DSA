3634a16df1a5d4b1e82ac29e0bbed2c047b54e5f
Gaurav Bhardwaj <smartgaurav22@gmail.com>
2022-10-06 10:40:13 +0530

    Formatting Issues

--- algorithms/JavaScript/src/trie/trie-implementation.js
+++ algorithms/JavaScript/src/trie/trie-implementation.js
@@ -1,71 +1,79 @@
 /*
-A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. 
-There are various applications of this data structure, such as autocomplete and spellchecker.
+A trie (pronounced as "try") or prefix tree is a tree data structure
+used to efficiently store and retrieve keys in a dataset of strings.
+There are various applications of this data structure,
+such as autocomplete and spellchecker.
 
+Insertion:
+Average Case: O(N)
+Worst Case: O(N)
+Best Case: O(N)
 
+Deletion:
+Average Case: O(N)
+Worst Case: O(N)
+Best Case: O(N)
 
-OPERATION	AVERAGE CASE	WORST CASE	BEST CASE
----------   ------------    ----------  ---------
-Insertion	O(N)	            O(N)	O(N)
-Deletion	O(N)	            O(N)	O(N)
-Searching	O(N)	            O(N)	O(1)
+Searching:
+Average Case: O(N)
+Worst Case: O(N)
+Best Case: O(1)
 
-Space complexity for creation of a trie: O(alphabet_size * average key length * N)
+Space complexity: O(alphabet_size * average key length * N)
 */
 
-
 /*
-Create a node that will have two properties — 
+Create a node that will have two properties —
 one is the hash map for storing children.
 the other one is for keeping track of the end of the word.
 */
 
 class Node {
-    constructor() {
-        this.children = {};
-        this.isEndWord = false;
-    }
+  constructor() {
+    this.children = {};
+    this.isEndWord = false;
+  }
 }
 
 class Trie {
-    constructor() {
-        this.root = new Node();
+  constructor() {
+    this.root = new Node();
+  }
+  insert(word) {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children[char]) {
+        node.children[char] = new Node();
+      }
+      node = node.children[char];
     }
-    insert(word) {
-        let node = this.root;
-        for (const char of word) {
-            if (!node.children[char]) {
-                node.children[char] = new Node();
-            }
-            node = node.children[char];
-        }
-        node.isEndWord = true;
+    node.isEndWord = true;
+  }
+  search(word) {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children[char]) {
+        return false;
+      }
+      node = node.children[char];
     }
-    search(word) {
-        let node = this.root;
-        for (const char of word) {
-            if (!node.children[char]) {
-                return false;
-            }
-            node = node.children[char];
-        }
-        return node.isEndWord ? true : false;
+    return node.isEndWord ? true : false;
+  }
+  startsWith(prefix) {
+    let node = this.root;
+    for (const char of prefix) {
+      if (!node.children[char]) {
+        return false;
+      }
+      node = node.children[char];
     }
-    startsWith(prefix) {
-        let node = this.root;
-        for (const char of prefix) {
-            if (!node.children[char]) {
-                return false;
-            }
-            node = node.children[char];
-        }
-        return true;
-    }
+    return true;
+  }
 }
 
 const trie = new Trie();
-trie.insert('datastructures')
-trie.insert('datablock')
-console.log(trie.search('dsa')) //false
-console.log(trie.search('datablock')) //true
-console.log(trie.startsWith('data')) //true
+trie.insert('datastructures');
+trie.insert('datablock');
+console.log(trie.search('dsa')); // false
+console.log(trie.search('datablock')); // true
+console.log(trie.startsWith('data')); // true
