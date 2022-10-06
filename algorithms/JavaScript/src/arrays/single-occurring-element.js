6f6ef9d61aa93832d93021b297048468c10d0964
Pranav-Rustagi <pranav001100@gmail.com>
2022-10-06 22:17:55 +0530

    Single occurring element among duplicates

---
+++ algorithms/JavaScript/src/arrays/single-occurring-element.js
@@ -0,0 +1,14 @@
+// Problem: Given an array of integers, every element appears twice except for one. Find that single one.
+// Space Complexity: O(1)
+// Time Complexity: O(n)
+
+function singleOccurringElement(arr) {
+    let result = 0;
+    for(let el of arr) {
+        result ^= el;
+    }
+    return result;
+}
+
+const arr = [2, 5, 7, 3, 1, 8, 8, 9, 4, 2, 7, 1, 4, 9, 5];
+console.log(singleOccurringElement(arr));
