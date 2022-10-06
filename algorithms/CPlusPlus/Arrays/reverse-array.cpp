5c8ed1bd415755ce7b3cc357941adafb104fd032
HARSH GUPTA <87202102+HarshGupta74@users.noreply.github.com>
2022-10-06 21:40:38 +0530

    updated code for reverse arrray

--- algorithms/CPlusPlus/Arrays/reverse-array.cpp
+++ algorithms/CPlusPlus/Arrays/reverse-array.cpp
@@ -1,33 +1,21 @@
-//Description : This program reverses array elements using two pointer method.
-//Time Complexity : O(n), where n is the array size
-//Auxiliary Space : O(1)
 #include<bits/stdc++.h>
-using namespace std ;
-void reverse_array(vector<int> arr){
-// Here we will use two pointer approach
-int low = 0 ;     //First pointer at the beginning of the array
-int high = arr.size()-1;  //Second pointer at the end of the array
-
-while(low < high){
-  //Swapping array items at the low and high index
-  swap(arr[low],arr[high]);
-  low++;  //Incrementing the low pointer
-  high--; //Decrementing the high pointer
-}
-
-  for(int i = 0; i < arr.size() ; i++){
-  cout<<arr[i]<<" ";
-  }
-}
-int main(){
-  int n ; //Size of the array
+using namespace std;
+int main()
+{
+  int n;
   cin>>n;
-  int item ;
-  std::vector<int> arr ;
-  while(n--){
-  cin>>item;
-  arr.push_back(item); 
-  }
-  reverse_array(arr) ; //Function call
-  return 0;
+    int arr[1000];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int i=0;
+    int j=n-1;
+    while(i<j){
+        swap(arr[i++],arr[j--]);
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+    
 }
