d68e5b14d0ed4bacd4490f6d627d0d69823fb8b4
Himanshu Hiware <91529311+himanshu-hiware@users.noreply.github.com>
2022-10-12 17:12:40 +0530

    DoublyLinkedList.java
    improved code with insertion at the end , beginning ,and at any position . Also I have added code for deletion the first and last node.

--- algorithms/Java/linked-lists/doubly.java
+++ algorithms/Java/linked-lists/doubly.java
@@ -1,77 +1,123 @@
-class DoublyLinkedList {    
-    class Node{  
-        int item;  
-        Node previous;  
-        Node next;  
-   
-        public Node(int item) {  
-            this.item = item;  
-        }  
-    }  
+public class DoublyLinkedList {
+    static class Node{
+        int data;
+        Node previous;
+        Node next;
+        public Node(int data){
+            this.data=data;
+        }
+    }
+    Node head,tail=null;
 
-    Node head, tail = null;  
-    
-    public void addNode(int item) { 
-        Node newNode = new Node(item);  
-   
-        //if list is empty, head and tail points to newNode  
-        if(head == null) {  
-            head = tail = newNode;  
-            //head's previous will be null  
-            head.previous = null;  
-            //tail's next will be null  
-            tail.next = null;  
-        }  
-        else {  
-            //add newNode to the end of list. tail->next set to newNode  
-            tail.next = newNode;  
-            //newNode->previous set to tail  
-            newNode.previous = tail;  
-            //newNode becomes new tail  
-            tail = newNode;  
-            //tail's next point to null  
-            tail.next = null;  
-        }  
-    }  
-     
-    public void printNodes() {   
-        Node current = head;  
-        if(head == null) {  
-            System.out.println("Doubly linked list is empty");  
-            return;  
-        }  
-        System.out.println("The doubly linked list is: ");
-        System.out.print("[ ");  
-        while(current != null) {    
-            System.out.print(current.item + " ");
-            //go to next  
-            current = current.next;  
-        } 
-        System.out.print("]"); 
-    }  
-}
- 
-public class doubly {
-    public static void main(String [] args){
-        DoublyLinkedList dl_List = new DoublyLinkedList();  
-        
-        dl_List.addNode(5);  
-        dl_List.addNode(7);  
-        dl_List.addNode(1);  
-        dl_List.addNode(17);  
-        dl_List.addNode(27);  
-   
-        dl_List.printNodes();
-        //this will print :
-        /*
-        The doubly linked list is: 
-        [ 5 7 1 17 27 ]
-        */
+    public void addInLast(int data){
+        Node newNode = new Node(data);
+        if(head==null){
+            //both head and tail will point to newNode
+            head=tail=newNode;
+            head.previous=null;
+            tail.next=null;
+        }
+        else{
+            tail.next=newNode;
+            newNode.previous=tail;
+            tail=newNode;
+        }
+    }
+    public void addAtFirst(int data){
+        Node newNode = new Node(data);
+        if(head==null){
+            //both head and tail will point to newNode
+            head=tail=newNode;
+            head.previous=null;
+            tail.next=null;
+        }
+        else{
+            newNode.next=head;
+            head.previous=newNode;
+            head=newNode;
+        }
+    }
+    public void addAtPos(int data,int pos){
+        Node temp=head;
+
+        Node newNode = new Node(data);
+        if(head==null){
+            //both head and tail will point to newNode
+            head=tail=newNode;
+            head.previous=null;
+            tail.next=null;
+        }
+        else{
+            Node temp1=head.next;
+            int i=0;
+            while(i<pos){
+                temp=temp.next;
+                temp1=temp1.next;
+                i++;
+            }
+            temp.next=newNode;
+            newNode.previous=temp;
+            newNode.next=temp1;
+            temp1.previous=newNode;
+        }
+    }
+    public void deleteLast(){
+
+        if(head==null){
+            System.out.println("the list is empty");
+        }
+        else {
+            tail=tail.previous;
+            tail.next=null;
+        }
+    }
+    public void deleteFirst(){
+
+        if(head==null){
+            System.out.println("the list is empty");
+        } else if (head==tail) {
+            tail=head=null;
+        } else {
+            head=head.next;
+            head.previous=null;
+        }
+    }
+    public void display(){
+        if(head==null){
+            System.out.println("list is empty");
+        }
+        Node curr=head;
+        while(curr!=null){
+            System.out.print(curr.data+"->");
+            curr=curr.next;
+        }
+        System.out.print("NULL");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList list1=new DoublyLinkedList();
+        list1.addInLast(1);
+        list1.addInLast(2);
+        list1.addInLast(3);
+        list1.addInLast(4);
+        list1.addInLast(5);
+        list1.addAtFirst(6);
+        list1.addAtFirst(7);
+        list1.addAtFirst(8);
+        list1.addAtPos(0,2);
+        list1.display();  //list after inserting the elements 
+        list1.deleteFirst();
+        list1.deleteFirst();
+        list1.display();  // list after deleting 1st node
+        list1.deleteLast();
+        list1.display(); // list after deleting last node 
     }
 }
 
 /*
-    to run the file:
-    javac doubly.java
-    java doubly
+output :
+8->7->6->0->1->2->3->4->5->NULL
+6->0->1->2->3->4->5->NULL
+6->0->1->2->3->4->NULL
 */
