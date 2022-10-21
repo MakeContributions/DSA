7a445d0e1334b68d496a2274feda5099d15d6c19
4kpros <prosper.abouar@gmail.com>
2022-10-22 00:23:11 +0100

    Add time complexity of Fibonacci with recursion

--- algorithms/CPlusPlus/Recursion/fibonacci.cpp
+++ algorithms/CPlusPlus/Recursion/fibonacci.cpp
@@ -1,20 +1,24 @@
 /*
     Description: Program to calculate fibonacci using recursion
+
+    Time complexity: O(2^n) where n is the number
 */
 #include <iostream>
 using namespace std;
 
+//Recursive function with integer parameter
 int RecursiveFibonacci(int number)
 {
-    if(number == 1 || number == 2)
+    if(number == 0 || number == 1)
         return 1;
 
-    if(number >= 3)
+    if(number >= 2)
         return RecursiveFibonacci(number - 1) + RecursiveFibonacci(number - 2);
         
     return 0;
 }
 
+//Main function
 int main(int argc, char *argv[])
 {
     int number = 10;
