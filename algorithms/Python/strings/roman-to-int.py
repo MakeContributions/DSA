4243bf5432d568fe06959607e98fccaad3fda332
Humberto Harris <hharris@techgenies.com>
2022-11-10 11:29:46 -0500

    Added time/space complexity and results

--- algorithms/Python/strings/roman-to-int.py
+++ algorithms/Python/strings/roman-to-int.py
@@ -1,6 +1,8 @@
 """
 This program converts a string that represents a valid roman number to its decimal equivalent.
 If the string is not a valid roman number, raises a ValueError exception.
+Time Complexity : O(n)
+Space Complexity : O(1)
 """
 
 equivalence = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -34,4 +36,6 @@ def roman_to_int(roman_number: str) -> int | ValueError:
 
 if __name__ == '__main__':
     print(roman_to_int('MCMXCIV'))
+    # 1994
     print(roman_to_int('abc'))
+    # ValueError: The string must contain only valid roman numbers: dict_keys(['I', 'V', 'X', 'L', 'C', 'D', 'M'])
