6d009da7647859c8b4bfde31c259ee363ab3ac16
RK-Shandilya <89962501+RK-Shandilya@users.noreply.github.com>
2022-12-15 15:00:42 +0530

    Update reverseInGroupsOfK.cpp

--- algorithms/CPlusPlus/Linked-Lists/reverseInGroupsOfK.cpp
+++ algorithms/CPlusPlus/Linked-Lists/reverseInGroupsOfK.cpp
@@ -7,7 +7,35 @@
 // Input: 1->2->3->4->5->6->7->8->NULL, K = 3 
 // Output: 3->2->1->6->5->4->8->7->NULL 
 
-Node* kReverse(Node* head, int k) {
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+};
+
+void push(Node* &head_ref, int new_data)
+{
+    Node* new_node = new Node();
+ 
+    new_node->data = new_data;
+ 
+    new_node->next = head_ref;
+ 
+    head_ref = new_node;
+}
+
+void printList(Node* node)
+{
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+}
+
+Node* kReverse(Node* &head, int k) {
   
     // base case
     if(head == NULL) {
@@ -31,4 +59,20 @@ Node* kReverse(Node* head, int k) {
         head -> next = kReverse(next,k);
     }
     return prev;
+}
+
+int main(){
+  Node* head=NULL;
+  Node* ans =head;
+  push(head,1);
+  push(head,2);
+  push(head,3);
+  push(head,4);
+  push(head,5);
+  push(head,6);
+  push(head,7);
+  push(head,8);
+  printList(head);
+  head = kReverse(head,3);
+  printList(head);
 }
