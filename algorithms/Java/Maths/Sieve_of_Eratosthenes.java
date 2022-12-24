d8272848fd18609ae6005fac6f813b7d1f6e2a79
mihirsuman0131@gmail.com <mihirsuman0131@gmail.com>
2022-12-24 15:48:52 +0530

    chore(Java): add sieve of eratosthenes #1117

--- algorithms/Java/Maths/Sieve_of_Eratosthenes.java
+++ algorithms/Java/Maths/Sieve_of_Eratosthenes.java
@@ -49,4 +49,8 @@ public class Sieve_of_Eratosthenes {
 /* Time Complexity: O(n*log(log(n)))
    Auxiliary Space: O(n)
    Constraint: n = 10,000,000
+
+   Input: 100
+   Output: Following are the prime numbers: 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 
+
  */
