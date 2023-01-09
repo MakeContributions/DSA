15c2ba93258d90c4153f04d4a93ca8ce4f58a70a
Luuk <luukberkers@protonmail.com>
2023-01-09 16:03:58 +0100

    Formatting

--- algorithms/CPlusPlus/Maths/palindrome.cpp
+++ algorithms/CPlusPlus/Maths/palindrome.cpp
@@ -3,28 +3,30 @@
 #include <string>
 
 int main() {
-    // User explanation and examples
-    std::cout << "Please type a string, e.g.: pop, civic, 123454321, rotor, obobo, nancyiycnan etc.\n";
+  // User explanation and examples
+  std::cout << "Please type a string, e.g.: pop, civic, 123454321, rotor, "
+               "obobo, nancyiycnan etc.\n";
 
-    // Define a buffer variable
-    std::string user_input;
-    // Taking input from user
-    std::cin >> user_input;
+  // Define a buffer variable
+  std::string user_input;
+  // Taking input from user
+  std::cin >> user_input;
 
-    // This will hold the reverse of the input
-    // We make sure it is as long as the input string and fill it with zero-bytes for now
-    std::string rev(user_input.length(), '\0');
-    // Copying the reverse of the string
-    std::reverse_copy(user_input.begin(), user_input.end(), rev.begin());
+  // This will hold the reverse of the input
+  // We make sure it is as long as the input string and fill it with zero-bytes
+  // for now
+  std::string rev(user_input.length(), '\0');
+  // Copying the reverse of the string
+  std::reverse_copy(user_input.begin(), user_input.end(), rev.begin());
 
-    // Compare and output
-    std::cout << (user_input == rev ? "Palindrome\n" : "Non-palindrome\n");
+  // Compare and output
+  std::cout << (user_input == rev ? "Palindrome\n" : "Non-palindrome\n");
 
-    // Exit with success
-    return 0;
+  // Exit with success
+  return 0;
 
-    // Complexity, with n = the length of the input:
-    //   Constructing rev is O(n)
-    //   std::reverse_copy is O(n)
-    //   Total: O(n)
+  // Complexity, with n = the length of the input:
+  //   Constructing rev is O(n)
+  //   std::reverse_copy is O(n)
+  //   Total: O(n)
 }
