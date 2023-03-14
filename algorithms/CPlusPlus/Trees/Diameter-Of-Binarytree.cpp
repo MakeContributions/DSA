c67272751f755f917c7ec15b0c2db276c8620f92
Revanth Naik <revanthsonu007@gmail.com>
2023-03-14 23:08:52 +0530

    Create Diameter-Of-Binarytree.cpp

---
+++ algorithms/CPlusPlus/Trees/Diameter-Of-Binarytree.cpp
@@ -0,0 +1,34 @@
+/*Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+
+Time Complexity : O(n);
+Space Complexity : O(n)*/
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    int height(TreeNode* root, int &diameter){
+        if(!root)
+            return 0;
+        int lh = height(root->left, diameter);
+        int rh = height(root->right, diameter);
+        
+        diameter = max(diameter, lh + rh);
+        return 1 + max(lh,rh);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        height(root, diameter);
