7af49b45c74437cc7ae2544714570c336a916192
_henriquearaujo98 <henrique.araujo@cybermap.eu>
2023-08-31 16:40:44 +0000

    Remove duplicates from array in C#

---
+++ algorithms/CSharp/src/Arrays/remove-duplicates-from-array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms.Arrays
+{
+    
+    public class RemoveDuplicatesFromArray
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+
+            int i = 1;
+
+            foreach (int n in nums)
+            {
+                if (nums[i - 1] != n) nums[i++] = n;
+            }
+
+            return i;
+        }
+
+        public static void Main()
+        {
+            int[] numbers = new int[] { 7, 1, 2, 3, 1, 7, 2 };
+
+            int arrayLength = RemoveDuplicates(numbers);
+
+            Console.WriteLine(numbers.ToList().Take(arrayLength));
+        }
+    }
+}
+
+/*
+ * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
+ * The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+ */
---
+++ algorithms/CSharp/test/Arrays/remove-duplicates-from-array.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.Tests.Arrays
+{
+    [TestFixture]
+    class remove_duplicates_from_array
+    {
+        static object[] TestCasesForRemoveDuplicatesFromArray =
+       {
+            new object[]
+            {
+                new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4},
+                new int[] { 0, 1, 2, 3, 4 }
+            },
+            new object[]
+            {
+                new int[] { 1, 1, 2},
+                new int[] { 1, 2}
+            },
+        };
+
+        [TestCaseSource(nameof(TestCasesForRemoveDuplicatesFromArray))]
+        public void TestSingleNumber_ShouldGetExpectedResult(int[] numbers, int[] expected)
+        {
+            int i = Algorithms.Arrays.RemoveDuplicatesFromArray.RemoveDuplicates(numbers);
+
+            int[] result = numbers
+                .ToList()
+                .Take(i)
+                .ToArray();
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
