e2ab5bc4e6e98ffea341a2037fe4524ffcce5b75
_henriquearaujo98 <henrique.araujo@cybermap.eu>
2023-08-31 16:44:49 +0000

    Added time and space complexity

--- algorithms/CSharp/src/Arrays/remove-duplicates-from-array.cs
+++ algorithms/CSharp/src/Arrays/remove-duplicates-from-array.cs
@@ -33,4 +33,6 @@ namespace Algorithms.Arrays
 /*
  * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
  * The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
  */
