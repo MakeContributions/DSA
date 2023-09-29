71c64bdd998d1f0deef09ecf2e63af50943ce37f
shanmukh-sai2003 <104721888+shanmukh-sai2003@users.noreply.github.com>
2023-09-29 23:10:39 +0530

    Add explaination, sample input and output for bfs algorithm

--- algorithms/Java/graphs/bfs.java
+++ algorithms/Java/graphs/bfs.java
@@ -1,5 +1,12 @@
 // Java progam to implement breadth first search(bfs) traversals
+// Approach 
+// Graph is represented in the form of adjacencyMatrix
+// In BFS source node is explored and all its  unvisited neighbour nodes are added to queue
+// Now the first node in Queue is explored and all its unvisited neighbour nodes are added to queue
+// This process is continued untill all the nodes are visited.
+
 import java.util.*;
+import java.util.Queue;
 
 public class bfs {
 
@@ -23,15 +30,7 @@ public class bfs {
 
     //Main method
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // number of nodes or vertices
-        System.out.print("Enter number of nodes: ");
-        int numberOfNodes = sc.nextInt();
-
-        // To store connections of all nodes
-        int adjacencyMatrix[][] = new int[numberOfNodes][numberOfNodes];
-
-        // Sample input for 5 nodes
+        // graph
         // nodes   0 1 2 3 4
         //   0     0 1 0 1 0
         //   1     1 0 1 1 0
@@ -39,6 +38,26 @@ public class bfs {
         //   3     1 1 0 0 0
         //   4     0 0 1 0 0
 
+        // Sample input
+        // Enter number of nodes: 5
+        // Enter 1 if edge is present else 0, with 0 and all other nodes: 0 1 0 1 0
+        // Enter 1 if edge is present else 0, with 1 and all other nodes: 1 0 1 1 0
+        // Enter 1 if edge is present else 0, with 2 and all other nodes: 0 1 0 0 1
+        // Enter 1 if edge is present else 0, with 3 and all other nodes: 1 1 0 0 0
+        // Enter 1 if edge is present else 0, with 4 and all other nodes: 0 0 1 0 0
+        // Enter source node: 0
+
+        // Sample output
+        // BFS traversal: 0 1 3 2 4
+
+
+        Scanner sc = new Scanner(System.in);
+        // number of nodes or vertices
+        System.out.print("Enter number of nodes: ");
+        int numberOfNodes = sc.nextInt();
+
+        // To store connections of all nodes
+        int adjacencyMatrix[][] = new int[numberOfNodes][numberOfNodes];
         for(int i = 0; i < numberOfNodes; i++) {
             System.out.print("Enter 1 if edge is present else 0, with " + i + " and all other nodes: ");
             for(int j = 0; j < numberOfNodes; j++) {
@@ -58,7 +77,6 @@ public class bfs {
             }
         }
         System.out.println();
-
         sc.close();
     }
 }
