df04933fa1ccd6d33bbbbc72c0e89d86138cc1b9
dakshbamola <129683957+dakshbamola@users.noreply.github.com>
2024-01-18 23:45:15 +0530

    Update armstrong.c

--- algorithms/C/maths/armstrong.c
+++ algorithms/C/maths/armstrong.c
@@ -14,20 +14,21 @@ int main(){                             //main function
         digit++;                        //counting number of digits in the input number
         temp=temp/10;
     }
-    printf("%d\n",digit);
+    printf("Number of digits: %d\n",digit);
     temp=num;
     while(temp>0){
         sum=sum+ (int)(pow(temp%10, digit)+0.5);    // checking the armstrong property of the number
         temp/=10;                                   // we extract a digit from number using (temp%10) then 
     }                                               // raise that digit to number of digits in the number using pow function
                                                     // (pow()+0.5) is used to increase precision
-    printf("%d\n",sum);
+    printf("Sum of powered digits: %d\n",sum);
     if(num==sum){                                   //checking if the new number is equal to the input number
-        printf("Armstrong");  
+        printf("Armstrong");
     }
     else{
         printf("Not Armstrong");
     }
+    return 0;
 }
 
 //         Input        ||       Output
