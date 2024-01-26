2dbbfe739693b481f2121d836a1223d578bb7c05
t-summ <=>
2024-01-26 17:48:41 -0500

    add more explanation to eea

--- algorithms/CPlusPlus/Maths/extended-euclidean-algorithm.cpp
+++ algorithms/CPlusPlus/Maths/extended-euclidean-algorithm.cpp
@@ -1,3 +1,7 @@
+// Sample Input and Output: 
+// 56 15
+/* The GCD of 56 and 15 is 1
+   56 * -4 + 15 * 15 = 1   */
 #include <iostream>
 #include <vector>
 
@@ -21,13 +25,19 @@ vector<vector<int> > Identity()
 // Input: positive integers a and b
 // Output: a vector containing the integers x and y such that ax + by = GCD(a, b) and the GCD of a and b
 //         vector is of the format [x, y, GCD(a, b)] 
-// Function performs the Extended Euclidean Algorithm. This utililizes linear algebra to solve the Bezout Identity.
+// Explanation: Function performs the Extended Euclidean Algorithm. This utililizes linear algebra to solve the Bezout Identity.
+//              Start the algorithm by augmenting the 2 by 2 identity matrix xy to the column vector r of the 2 remainders such 
+//              that xy*[a, b] = r. Therefore, you begin with the system of two equations such that 1*a+0*b=a and 0*a+1*b=b
+//              which is obviously true. From there, the algorithm iteratively performs row operations on the two rows based on 
+//              which row has a larger remainder until a remainder is 0. Each row operation is subtracting the quotient q * smaller
+//              row to get the new remainder and the last one before a remainder is 0 is the GCD of the two starting remainders. 
+//              Further since xy*[a,b]=r still is unchanged after row operations, the solution for Bezout's Identity is the row 
+//              that isn't the 0 remainder. 
 vector<int> EEA(int a, int b)
 {
     vector<vector<int> > xy = Identity() ; // start with the Identity matrix to perform operations on 
+
     // initialize the remainder vector r 
-    // notice how column 1 represents the value being multiplied by a and col 2 represents being multiplied by b to get each solution in r
-    // therefore so far the first row has the solution 1*a + 0*b = a and the second has 0*a + 1*b = b which is true 
     vector<int> r ;
     r.push_back(a) ; 
     r.push_back(b) ;  
@@ -38,18 +48,20 @@ vector<int> EEA(int a, int b)
     // while no remainder is 0 
     while (r[0] != 0 && r[1] != 0)
     {
-        if (r[0] > r[1])
+        if (r[0] > r[1]) // if row 1 remainder is larger than row 2
         {
             q = r[0] / r[1] ; // get the quotient value 
-            r[0] = r[0] % r[1] ; // update the remainder 
-            xy[0][0] -= (q * xy[1][0]) ; // perform row operations on matrix by subtracting q*row 2 from row 1
+            // subtract q*row 2 from row 1 and update the values in row 1 to the result on matrix and remainder vector
+            r[0] -= (q * r[1]) ;  
+            xy[0][0] -= (q * xy[1][0]) ; 
             xy[0][1] -= (q * xy[1][1]) ;  
         }
-        else
+        else // row 2 remainder is larger than row 1 
         {
             q = r[1] / r[0] ; // get the quotient value 
-            r[1] = r[1] % r[0] ; // update the remainder
-            xy[1][0] -= (q * xy[0][0]) ; // perform row operations on matrix by subtracting q*row 1 from row 2
+            // subtract q*row 1 from row 2 and update the values in row 2 to the result on matrix and remainder vector
+            r[1] -= (q * r[0]) ; 
+            xy[1][0] -= (q * xy[0][0]) ; 
             xy[1][1] -= (q * xy[0][1]) ; 
         }
     }
@@ -84,7 +96,7 @@ int main(int argc, char **argv)
 {
     cout << "Enter two positive integers: " ; 
     int a, b ;
-    cin >> a >> b ; 
+    cin >> a >> b ;
     vector<int> out = EEA(a, b) ; 
     cout << "The GCD of " << a << " and " << b << " is " << out[2] << endl ; 
     cout << a << " * " << out[0] << " + " << b << " * " << out[1] << " = " << out[2] << endl ;
